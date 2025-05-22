5d087910607a6fb8076cbde389d8d708f3ea0f63
KenMirwell <kenmirwell@gmail.com>
2025-05-22 08:31:41 +0800

    update

--- page-annual-event-registration.php
+++ page-annual-event-registration.php
@@ -68,6 +68,19 @@ get_header();
     console.log("email", email)
     console.log("Generated Token:", token);
 
+    fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
+      method: 'POST',
+      headers: {
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
+        'Content-Type': 'application/json',
+        'Prefer': 'return=minimal'
+      },
+      body: JSON.stringify({ email, token })
+    }).then(response => {
+        console.log("getting the response")
+    })
+
 
     fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
       method: 'POST',
@@ -81,21 +94,21 @@ get_header();
     }).then(response => {
         console.log("response", response)
         console.log("response", response.body)
-      if (response.ok) {
-        // window.location.href = `/code-verification/?email=${encodeURIComponent(email)}`;
-        emailjs.send('service_1qkyi2i', 'template_d71x79v', {
-            email: email,
-            verification_link: "https://beige-fly-587526.hostingersite.com/annual-event-registration?t=" + token
-        })
-            .then(function() {
-                alert('Email sent successfully!');
-            }, function(error) {
-                console.error('FAILED...', error);
-                alert('Email failed to send!');
-    });
-      } else {
-        alert('Failed to store code.');
-      }
+    //   if (response.ok) {
+    //     // window.location.href = `/code-verification/?email=${encodeURIComponent(email)}`;
+    //     emailjs.send('service_1qkyi2i', 'template_d71x79v', {
+    //         email: email,
+    //         verification_link: "https://beige-fly-587526.hostingersite.com/annual-event-registration?t=" + token
+    //     })
+    //         .then(function() {
+    //             alert('Email sent successfully!');
+    //         }, function(error) {
+    //             console.error('FAILED...', error);
+    //             alert('Email failed to send!');
+    //         });
+    //   } else {
+    //     alert('Failed to store code.');
+    //   }
     });
   });
 </script>
