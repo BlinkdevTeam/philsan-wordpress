bd2ea93ce080573061253b98f6cdc20072b7e3b5
KenMirwell <kenmirwell@gmail.com>
2025-05-22 08:25:24 +0800

    update

--- page-token-verified.php
+++ page-token-verified.php
@@ -8,33 +8,7 @@ get_header();
 
 <div class="mx-auto w-[1280px]">
     <div class="flex w-[100%] py-[20px] h-[100vh]">
-        <div class="bg-[#F6F5F3] w-[50%] p-[50px] flex justify-center items-center">
-            <div class="flex flex-col gap-[20px]">
-                <h2 class="text-[38px]">Philsan</h2>
-                <h1 class="text-[48px] font-bold">Philippine Society of Animal Nutritionists' 38ᵗʰ ANNUAL CONVENTION</h1>
-                <p class="text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-         <div class="w-[50%]">
-            <form id="email-verification" class="text-black flex flex-col justify-center">
-                <div class="w-auto h-auto flex flex-col justify-center px-8 md:px-20 lg:px-24 py-12 rounded space-y-6 text-start">
-                    <div class="flex flex-col">
-                        <p class="sub-bi-heading text-[#344054]">Your email</p>
-                        <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            required
-                            placeholder="Email address"
-                            class="w-full p-3 border"
-                        />
-                    </div>
-                    <div>
-                        <button type="submit" class="hover:bg-[#32bd49] py-3 w-[148px] h-[60px] submit bg-[#959595] rounded-[8px] text-[#ffffff] cursor-pointer">Submit</button>
-                    </div>
-                </div>
-            </form>
-        </div>
+        <h2 class="text-[42px]">Token Verified!!!</h2>
     </div>
 </div>
 
@@ -80,7 +54,6 @@ get_header();
       body: JSON.stringify({ email, token })
     }).then(response => {
         console.log("response", response)
-        console.log("response", response.body)
       if (response.ok) {
         // window.location.href = `/code-verification/?email=${encodeURIComponent(email)}`;
         emailjs.send('service_1qkyi2i', 'template_d71x79v', {
@@ -92,7 +65,7 @@ get_header();
             }, function(error) {
                 console.error('FAILED...', error);
                 alert('Email failed to send!');
-    });
+      });
       } else {
         alert('Failed to store code.');
       }
