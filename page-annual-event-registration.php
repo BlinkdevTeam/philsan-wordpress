629c053adc297f84a726f3c182adbe99793574d0
KenMirwell <kenmirwell@gmail.com>
2025-05-22 16:01:19 +0800

    update

--- page-annual-event-registration.php
+++ page-annual-event-registration.php
@@ -269,7 +269,7 @@ get_header();
     console.log("Sponsored Registration:", sponsored);
 
 
-    fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
+    fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_registration_2025', {
       method: 'POST',
       headers: {
         'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
