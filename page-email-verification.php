fcd3130eadd27c35e4f6bf413d5adcb2b9abf16e
KenMirwell <kenmirwell@gmail.com>
2025-05-23 15:48:11 +0800

    Update

--- page-email-verification.php
+++ page-email-verification.php
@@ -70,7 +70,7 @@ get_header();
       console.log("Generated Token:", token);
 
       //Filter the email from the registration database
-      fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
+      fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_registration_2025', {
         method: 'GET',
         headers: {
           'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
