81aeef736cba7983fcb9e9d30394d749f5958d01
KenMirwell <kenmirwell@gmail.com>
2025-05-23 15:32:11 +0800

    Update

--- page-email-verification.php
+++ page-email-verification.php
@@ -28,6 +28,7 @@ get_header();
                             placeholder="Email address"
                             class="w-full p-3 border"
                         />
+                        <p class="email-exist hidden text-[red]">Email already in use</p>
                     </div>
                     <div>
                         <button type="submit" class="hover:bg-[#32bd49] py-3 w-[148px] h-[60px] submit bg-[#959595] rounded-[8px] text-[#ffffff] cursor-pointer">Submit</button>
@@ -79,39 +80,43 @@ get_header();
       })
       .then(response => response.json())
       .then(data => {
-        console.log("Matching data:", data);
+          const matchData = data.find(i => i.email === email);
+          if(!matchData) {
+            const elem = document.querySelector(".email-exist");
+            if (elem) el.classList.remove("hidden");
+            return;
+          }
+          //send POST request to email verification database
+          fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
+            method: 'POST',
+            headers: {
+              'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
+              'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
+              'Content-Type': 'application/json',
+              'Prefer': 'return=minimal'
+            },
+            body: JSON.stringify({ email, token: token+email })
+          }).then(response => {
+            if (response.ok) {
+              // window.location.href = `/code-verification/?email=${encodeURIComponent(email)}`;
+              emailjs.send('service_1qkyi2i', 'template_d71x79v', {
+                  email: email,
+                  verification_link: "https://beige-fly-587526.hostingersite.com/annual-event-registration?t=" + token + email
+              })
+                  .then(function() {
+                      // alert('Email sent successfully!');
+                      window.location.href = "https://beige-fly-587526.hostingersite.com/visit-email";
+                  }, function(error) {
+                      console.error('FAILED...', error);
+                      alert('Email failed to send!');
+                  });
+            } else {
+              alert('Failed to store code.');
+            }
+          });
       })
       .catch(error => {
         console.error("Error fetching data:", error);
-      });
-
-      //send POST request to email verification database
-      fetch('https://shvutlcgljqiidqxqrru.supabase.co/rest/v1/philsan_email_verification', {
-        method: 'POST',
-        headers: {
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnV0bGNnbGpxaWlkcXhxcnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTM2NDgsImV4cCI6MjA2MTQ4OTY0OH0.UXJKk6iIyaVJsohEB6CwwauC21YPez1xwsOFy9qa34Q',
-          'Content-Type': 'application/json',
-          'Prefer': 'return=minimal'
-        },
-        body: JSON.stringify({ email, token: token+email })
-      }).then(response => {
-        if (response.ok) {
-          // window.location.href = `/code-verification/?email=${encodeURIComponent(email)}`;
-          emailjs.send('service_1qkyi2i', 'template_d71x79v', {
-              email: email,
-              verification_link: "https://beige-fly-587526.hostingersite.com/annual-event-registration?t=" + token + email
-          })
-              .then(function() {
-                  // alert('Email sent successfully!');
-                  window.location.href = "https://beige-fly-587526.hostingersite.com/visit-email";
-              }, function(error) {
-                  console.error('FAILED...', error);
-                  alert('Email failed to send!');
-              });
-        } else {
-          alert('Failed to store code.');
-        }
       });
     });
 </script>
