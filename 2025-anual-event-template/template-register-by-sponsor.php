8a606dfa90069a11560d46947374e1951d1b3e21
KenMirwell <kenmirwell@gmail.com>
2025-06-04 14:54:33 +0800

    update

---
+++ 2025-anual-event-template/template-register-by-sponsor.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+*Template Name: Register By Sponsor
+*Template Post Type: 38th-convention
+*/
+
+get_header(); 
+
+?>
+
+<div class="mx-auto w-[1280px] flex-grow">
+    <div class="flex w-[100%] py-[20px]">
+        <div class="w-[100%]">
+            <h2 class="text-xl font-semibold text-green-700">Your Sponsor will Complete your Registration</h2>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
