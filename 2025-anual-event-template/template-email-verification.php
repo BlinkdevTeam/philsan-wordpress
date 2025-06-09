58c99ab9abf7edfcf445cf29ff62963a5ec521bd
KenMirwell <kenmirwell@gmail.com>
2025-06-09 13:08:20 +0800

    update

--- 2025-anual-event-template/template-email-verification.php
+++ 2025-anual-event-template/template-email-verification.php
@@ -103,11 +103,11 @@ get_header();
 
           console.log("matchData", matchData)
           
-          if (matchData.reg_status === "approved") {
+          if (matchData[reg_status] === "approved") {
             emailExistEl.classList.remove("hidden");
             emailPendingEl.classList.add("hidden");
             return
-          } else if (matchData.reg_status === "pending") {
+          } else if (matchData[reg_status] === "pending") {
             emailExistEl.classList.add("hidden");
             emailPendingEl.classList.remove("hidden");
 
