3ca5eac5235edad412c94e1df54323569cf364e3
KenMirwell <kenmirwell@gmail.com>
2025-06-09 14:42:56 +0800

    update

--- 2025-anual-event-template/template-registration-successful.php
+++ 2025-anual-event-template/template-registration-successful.php
@@ -11,7 +11,7 @@ get_header();
 <div class="mx-auto w-[1280px] flex-grow">
     <div class="flex w-[100%] py-[20px]">
         <div class="w-[100%]">
-            <h2 class="text-xl font-semibold text-green-700">Registration Successful</h2>
+            <h2 class="text-xl font-semibold text-green-700">Registration Sent</h2>
             <p class="mt-2 text-gray-700">
                 Thank you for registering. The PHILSAN team will review your submission and get back to you within 1–2 working days.
             </p>
