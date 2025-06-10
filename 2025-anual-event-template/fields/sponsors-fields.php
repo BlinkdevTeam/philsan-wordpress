93e3f5a9bdbb8b5862b1b6e7af9c9a6028f17b0d
KenMirwell <kenmirwell@gmail.com>
2025-06-10 12:57:11 +0800

    updated

--- 2025-anual-event-template/fields/sponsors-fields.php
+++ 2025-anual-event-template/fields/sponsors-fields.php
@@ -124,121 +124,122 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-3 gap-x-6">
         <div>
-        <input type="radio" id="no-sponsor" name="sponsor" value="A Company" required />
-        <label for="no-sponsor">No Sponsor</label>
+            <input type="radio" id="no-sponsor" name="sponsor" value="No Sponsor" required />
+            <label for="no-sponsor">No Sponsor</label>
         </div>
         <div>
-        <input type="radio" id="alltech" name="sponsor" value="A Company" required />
-        <label for="alltech">Alltech</label>
+            <input type="radio" id="alltech" name="sponsor" value="Alltech" />
+            <label for="alltech">Alltech</label>
         </div>
         <div>
-        <input type="radio" id="ariela" name="sponsor" value="B Company" />
-        <label for="ariela">Ariela</label>
+            <input type="radio" id="ariela" name="sponsor" value="Ariela" />
+            <label for="ariela">Ariela</label>
         </div>
         <div>
-        <input type="radio" id="awap" name="sponsor" value="C Company" />
-        <label for="awap">AWAP</label>
+            <input type="radio" id="awap" name="sponsor" value="AWAP" />
+            <label for="awap">AWAP</label>
         </div>
         <div>
-        <input type="radio" id="bounty_plus" name="sponsor" value="D Company" />
-        <label for="bounty_plus">Bounty Plus</label>
+            <input type="radio" id="bounty_plus" name="sponsor" value="Bounty Plus" />
+            <label for="bounty_plus">Bounty Plus</label>
         </div>
         <div>
-        <input type="radio" id="camden" name="sponsor" value="E Company" />
-        <label for="camden">Camden</label>
+            <input type="radio" id="camden" name="sponsor" value="Camden" />
+            <label for="camden">Camden</label>
         </div>
         <div>
-        <input type="radio" id="docinex" name="sponsor" value="E Company" />
-        <label for="docinex">Diconex</label>
+            <input type="radio" id="docinex" name="sponsor" value="Docinex" />
+            <label for="docinex">Docinex</label>
         </div>
         <div>
-        <input type="radio" id="dsm" name="sponsor" value="E Company" />
-        <label for="dsm">DSM</label>
+            <input type="radio" id="dsm" name="sponsor" value="DSM" />
+            <label for="dsm">DSM</label>
         </div>
         <div>
-        <input type="radio" id="east-asia" name="sponsor" value="E Company" />
-        <label for="east-asia">East Asia</label>
+            <input type="radio" id="east-asia" name="sponsor" value="East Asia" />
+            <label for="east-asia">East Asia</label>
         </div>
         <div>
-        <input type="radio" id="easybio" name="sponsor" value="E Company" />
-        <label for="easybio">Easybio</label>
+            <input type="radio" id="easybio" name="sponsor" value="Easybio" />
+            <label for="easybio">Easybio</label>
         </div>
         <div>
-        <input type="radio" id="equilivet" name="sponsor" value="E Company" />
-        <label for="equilivet">Equilivet</label>
+            <input type="radio" id="equilivet" name="sponsor" value="Equilivet" />
+            <label for="equilivet">Equilivet</label>
         </div>
         <div>
-        <input type="radio" id="evonik" name="sponsor" value="E Company" />
-        <label for="evonik">Evonik</label>
+            <input type="radio" id="evonik" name="sponsor" value="Evonik" />
+            <label for="evonik">Evonik</label>
         </div>
         <div>
-        <input type="radio" id="Inphilco" name="sponsor" value="E Company" />
-        <label for="Inphilco">Inphilco</label>
+            <input type="radio" id="inphilco" name="sponsor" value="Inphilco" />
+            <label for="inphilco">Inphilco</label>
         </div>
         <div>
-        <input type="radio" id="Interfarma" name="sponsor" value="E Company" />
-        <label for="Interfarma">Interfarma</label>
+            <input type="radio" id="interfarma" name="sponsor" value="Interfarma" />
+            <label for="interfarma">Interfarma</label>
         </div>
         <div>
-        <input type="radio" id="jcs" name="sponsor" value="E Company" />
-        <label for="jcs">JCS</label>
+            <input type="radio" id="jcs" name="sponsor" value="JCS" />
+            <label for="jcs">JCS</label>
         </div>
         <div>
-        <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="E Company" />
-        <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
+            <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="Jebsen and Jessen" />
+            <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
         </div>
         <div>
-        <input type="radio" id="kpp-powers" name="sponsor" value="E Company" />
-        <label for="kpp-powers">KPP Powers</label>
+            <input type="radio" id="kpp-powers" name="sponsor" value="KPP Powers" />
+            <label for="kpp-powers">KPP Powers</label>
         </div>
         <div>
-        <input type="radio" id="maxion" name="sponsor" value="E Company" />
-        <label for="maxion">Maxion</label>
+            <input type="radio" id="maxion" name="sponsor" value="Maxion" />
+            <label for="maxion">Maxion</label>
         </div>
         <div>
-        <input type="radio" id="mgb" name="sponsor" value="E Company" />
-        <label for="mgb">MGB</label>
+            <input type="radio" id="mgb" name="sponsor" value="MGB" />
+            <label for="mgb">MGB</label>
         </div>
         <div>
-        <input type="radio" id="nuevo-milenio" name="sponsor" value="E Company" />
-        <label for="nuevo-milenio">Nuevo Milenio</label>
+            <input type="radio" id="nuevo-milenio" name="sponsor" value="Nuevo Milenio" />
+            <label for="nuevo-milenio">Nuevo Milenio</label>
         </div>
         <div>
-        <input type="radio" id="novus" name="sponsor" value="E Company" />
-        <label for="novus">Novus</label>
+            <input type="radio" id="novus" name="sponsor" value="Novus" />
+            <label for="novus">Novus</label>
         </div>
         <div>
-        <input type="radio" id="philcema" name="sponsor" value="E Company" />
-        <label for="philcema">Philcema</label>
+            <input type="radio" id="philcema" name="sponsor" value="Philcema" />
+            <label for="philcema">Philcema</label>
         </div>
         <div>
-        <input type="radio" id="promax" name="sponsor" value="E Company" />
-        <label for="promax">Promax</label>
+            <input type="radio" id="promax" name="sponsor" value="Promax" />
+            <label for="promax">Promax</label>
         </div>
         <div>
-        <input type="radio" id="realvet" name="sponsor" value="E Company" />
-        <label for="realvet">Realvet</label>
+            <input type="radio" id="realvet" name="sponsor" value="Realvet" />
+            <label for="realvet">Realvet</label>
         </div>
         <div>
-        <input type="radio" id="rnt" name="sponsor" value="E Company" />
-        <label for="rnt">RNT</label>
+            <input type="radio" id="rnt" name="sponsor" value="RNT" />
+            <label for="rnt">RNT</label>
         </div>
         <div>
-        <input type="radio" id="seastemas" name="sponsor" value="E Company" />
-        <label for="seastemas">Seastemas</label>
+            <input type="radio" id="seastemas" name="sponsor" value="Seastemas" />
+            <label for="seastemas">Seastemas</label>
         </div>
         <div>
-        <input type="radio" id="st-john-biomedics" name="sponsor" value="E Company" />
-        <label for="st-john-biomedics">St. John Biomedics</label>
+            <input type="radio" id="st-john-biomedics" name="sponsor" value="St. John Biomedics" />
+            <label for="st-john-biomedics">St. John Biomedics</label>
         </div>
         <div>
-        <input type="radio" id="vet-health" name="sponsor" value="E Company" />
-        <label for="vet-health">Vet Health</label>
+            <input type="radio" id="vet-health" name="sponsor" value="Vet Health" />
+            <label for="vet-health">Vet Health</label>
         </div>
         <div>
-        <input type="radio" id="vet-specialist" name="sponsor" value="E Company" />
-        <label for="vet-specialist">Vet Specialist</label>
+            <input type="radio" id="vet-specialist" name="sponsor" value="Vet Specialist" />
+            <label for="vet-specialist">Vet Specialist</label>
         </div>
+
     </div>
     </div>
 
