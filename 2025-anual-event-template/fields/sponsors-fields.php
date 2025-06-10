6cfaa383e358a32f831eb19d475a9462b72d679d
KenMirwell <kenmirwell@gmail.com>
2025-06-10 11:03:14 +0800

    update

--- 2025-anual-event-template/fields/sponsors-fields.php
+++ 2025-anual-event-template/fields/sponsors-fields.php
@@ -120,136 +120,138 @@
     </div>
 </div> -->
 
-<div class="flex flex-col">
-  <p class="sub-bi-heading text-[#344054] mb-4">Who's your sponsor?</p>
+<div className="relative">
+    <div class="flex flex-col">
+    <p class="sub-bi-heading text-[#344054] mb-4">Who's your sponsor?</p>
 
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-3 gap-x-6">
-    <div>
-      <input type="radio" id="no-sponsor" name="sponsor" value="A Company" required />
-      <label for="no-sponsor">No Sponsor</label>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-3 gap-x-6">
+        <div>
+        <input type="radio" id="no-sponsor" name="sponsor" value="A Company" required />
+        <label for="no-sponsor">No Sponsor</label>
+        </div>
+        <div>
+        <input type="radio" id="alltech" name="sponsor" value="A Company" required />
+        <label for="alltech">Alltech</label>
+        </div>
+        <div>
+        <input type="radio" id="ariela" name="sponsor" value="B Company" />
+        <label for="ariela">Ariela</label>
+        </div>
+        <div>
+        <input type="radio" id="awap" name="sponsor" value="C Company" />
+        <label for="awap">AWAP</label>
+        </div>
+        <div>
+        <input type="radio" id="bounty_plus" name="sponsor" value="D Company" />
+        <label for="bounty_plus">Bounty Plus</label>
+        </div>
+        <div>
+        <input type="radio" id="camden" name="sponsor" value="E Company" />
+        <label for="camden">Camden</label>
+        </div>
+        <div>
+        <input type="radio" id="docinex" name="sponsor" value="E Company" />
+        <label for="docinex">Diconex</label>
+        </div>
+        <div>
+        <input type="radio" id="dsm" name="sponsor" value="E Company" />
+        <label for="dsm">DSM</label>
+        </div>
+        <div>
+        <input type="radio" id="east-asia" name="sponsor" value="E Company" />
+        <label for="east-asia">East Asia</label>
+        </div>
+        <div>
+        <input type="radio" id="easybio" name="sponsor" value="E Company" />
+        <label for="easybio">Easybio</label>
+        </div>
+        <div>
+        <input type="radio" id="equilivet" name="sponsor" value="E Company" />
+        <label for="equilivet">Equilivet</label>
+        </div>
+        <div>
+        <input type="radio" id="evonik" name="sponsor" value="E Company" />
+        <label for="evonik">Evonik</label>
+        </div>
+        <div>
+        <input type="radio" id="Inphilco" name="sponsor" value="E Company" />
+        <label for="Inphilco">Inphilco</label>
+        </div>
+        <div>
+        <input type="radio" id="Interfarma" name="sponsor" value="E Company" />
+        <label for="Interfarma">Interfarma</label>
+        </div>
+        <div>
+        <input type="radio" id="jcs" name="sponsor" value="E Company" />
+        <label for="jcs">JCS</label>
+        </div>
+        <div>
+        <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="E Company" />
+        <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
+        </div>
+        <div>
+        <input type="radio" id="kpp-powers" name="sponsor" value="E Company" />
+        <label for="kpp-powers">KPP Powers</label>
+        </div>
+        <div>
+        <input type="radio" id="maxion" name="sponsor" value="E Company" />
+        <label for="maxion">Maxion</label>
+        </div>
+        <div>
+        <input type="radio" id="mgb" name="sponsor" value="E Company" />
+        <label for="mgb">MGB</label>
+        </div>
+        <div>
+        <input type="radio" id="nuevo-milenio" name="sponsor" value="E Company" />
+        <label for="nuevo-milenio">Nuevo Milenio</label>
+        </div>
+        <div>
+        <input type="radio" id="novus" name="sponsor" value="E Company" />
+        <label for="novus">Novus</label>
+        </div>
+        <div>
+        <input type="radio" id="philcema" name="sponsor" value="E Company" />
+        <label for="philcema">Philcema</label>
+        </div>
+        <div>
+        <input type="radio" id="promax" name="sponsor" value="E Company" />
+        <label for="promax">Promax</label>
+        </div>
+        <div>
+        <input type="radio" id="realvet" name="sponsor" value="E Company" />
+        <label for="realvet">Realvet</label>
+        </div>
+        <div>
+        <input type="radio" id="rnt" name="sponsor" value="E Company" />
+        <label for="rnt">RNT</label>
+        </div>
+        <div>
+        <input type="radio" id="seastemas" name="sponsor" value="E Company" />
+        <label for="seastemas">Seastemas</label>
+        </div>
+        <div>
+        <input type="radio" id="st-john-biomedics" name="sponsor" value="E Company" />
+        <label for="st-john-biomedics">St. John Biomedics</label>
+        </div>
+        <div>
+        <input type="radio" id="vet-health" name="sponsor" value="E Company" />
+        <label for="vet-health">Vet Health</label>
+        </div>
+        <div>
+        <input type="radio" id="vet-specialist" name="sponsor" value="E Company" />
+        <label for="vet-specialist">Vet Specialist</label>
+        </div>
     </div>
-    <div>
-      <input type="radio" id="alltech" name="sponsor" value="A Company" required />
-      <label for="alltech">Alltech</label>
     </div>
-    <div>
-      <input type="radio" id="ariela" name="sponsor" value="B Company" />
-      <label for="ariela">Ariela</label>
-    </div>
-    <div>
-      <input type="radio" id="awap" name="sponsor" value="C Company" />
-      <label for="awap">AWAP</label>
-    </div>
-    <div>
-      <input type="radio" id="bounty_plus" name="sponsor" value="D Company" />
-      <label for="bounty_plus">Bounty Plus</label>
-    </div>
-    <div>
-      <input type="radio" id="camden" name="sponsor" value="E Company" />
-      <label for="camden">Camden</label>
-    </div>
-    <div>
-      <input type="radio" id="docinex" name="sponsor" value="E Company" />
-      <label for="docinex">Diconex</label>
-    </div>
-    <div>
-      <input type="radio" id="dsm" name="sponsor" value="E Company" />
-      <label for="dsm">DSM</label>
-    </div>
-    <div>
-      <input type="radio" id="east-asia" name="sponsor" value="E Company" />
-      <label for="east-asia">East Asia</label>
-    </div>
-    <div>
-      <input type="radio" id="easybio" name="sponsor" value="E Company" />
-      <label for="easybio">Easybio</label>
-    </div>
-    <div>
-      <input type="radio" id="equilivet" name="sponsor" value="E Company" />
-      <label for="equilivet">Equilivet</label>
-    </div>
-    <div>
-      <input type="radio" id="evonik" name="sponsor" value="E Company" />
-      <label for="evonik">Evonik</label>
-    </div>
-    <div>
-      <input type="radio" id="Inphilco" name="sponsor" value="E Company" />
-      <label for="Inphilco">Inphilco</label>
-    </div>
-    <div>
-      <input type="radio" id="Interfarma" name="sponsor" value="E Company" />
-      <label for="Interfarma">Interfarma</label>
-    </div>
-    <div>
-      <input type="radio" id="jcs" name="sponsor" value="E Company" />
-      <label for="jcs">JCS</label>
-    </div>
-    <div>
-      <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="E Company" />
-      <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
-    </div>
-    <div>
-      <input type="radio" id="kpp-powers" name="sponsor" value="E Company" />
-      <label for="kpp-powers">KPP Powers</label>
-    </div>
-    <div>
-      <input type="radio" id="maxion" name="sponsor" value="E Company" />
-      <label for="maxion">Maxion</label>
-    </div>
-    <div>
-      <input type="radio" id="mgb" name="sponsor" value="E Company" />
-      <label for="mgb">MGB</label>
-    </div>
-    <div>
-      <input type="radio" id="nuevo-milenio" name="sponsor" value="E Company" />
-      <label for="nuevo-milenio">Nuevo Milenio</label>
-    </div>
-    <div>
-      <input type="radio" id="novus" name="sponsor" value="E Company" />
-      <label for="novus">Novus</label>
-    </div>
-    <div>
-      <input type="radio" id="philcema" name="sponsor" value="E Company" />
-      <label for="philcema">Philcema</label>
-    </div>
-    <div>
-      <input type="radio" id="promax" name="sponsor" value="E Company" />
-      <label for="promax">Promax</label>
-    </div>
-    <div>
-      <input type="radio" id="realvet" name="sponsor" value="E Company" />
-      <label for="realvet">Realvet</label>
-    </div>
-    <div>
-      <input type="radio" id="rnt" name="sponsor" value="E Company" />
-      <label for="rnt">RNT</label>
-    </div>
-    <div>
-      <input type="radio" id="seastemas" name="sponsor" value="E Company" />
-      <label for="seastemas">Seastemas</label>
-    </div>
-    <div>
-      <input type="radio" id="st-john-biomedics" name="sponsor" value="E Company" />
-      <label for="st-john-biomedics">St. John Biomedics</label>
-    </div>
-    <div>
-      <input type="radio" id="vet-health" name="sponsor" value="E Company" />
-      <label for="vet-health">Vet Health</label>
-    </div>
-    <div>
-      <input type="radio" id="vet-specialist" name="sponsor" value="E Company" />
-      <label for="vet-specialist">Vet Specialist</label>
-    </div>
-  </div>
-</div>
 
-<div id="upload-field" class="flex flex-col hidden">
-    <p class="sub-bi-heading text-[#344054]">Please upload your proof of payment</p>
-    <div id="upload-area" class="flex items-center justify-center w-[100%] p-[50px] rounded-[20px] bg-[#e2e1e1] cursor-pointer">
-        <span id="upload-text">Upload</span>
+    <div id="upload-field" class="flex flex-col hidden absolute bottom-[0px] right-0">
+        <p class="sub-bi-heading text-[#344054]">Please upload your proof of payment</p>
+        <div id="upload-area" class="flex items-center justify-center w-[100%] p-[50px] rounded-[20px] bg-[#e2e1e1] cursor-pointer">
+            <span id="upload-text">Upload</span>
+        </div>
+            <!-- Hidden file input -->
+        <input id="upload-input" type="file" id="file-input" class="hidden" accept="image/*" required/>
     </div>
-        <!-- Hidden file input -->
-    <input id="upload-input" type="file" id="file-input" class="hidden" accept="image/*" required/>
 </div>
 
 <script type="text/javascript"
@@ -262,7 +264,7 @@
 
     radios.forEach(radio => {
         radio.addEventListener("change", () => {
-        if (radio.id === "alltech" && radio.checked) {
+        if (radio.id === "no-sponsor" && !radio.checked) {
             agreementContainer.classList.remove("hidden");
             agreementInput.setAttribute("required", "true");
         } else {
