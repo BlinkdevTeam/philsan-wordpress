9ecd68ee94db666255aff1b65525a81054095b29
KenMirwell <kenmirwell@gmail.com>
2025-06-10 15:10:04 +0800

    updated

---
+++ 2025-anual-event-template/fields/other-checkboxes.php
@@ -0,0 +1,66 @@
+<div class="flex flex-col">
+    <p class="sub-bi-heading text-[#344054]">Are you a PHILSAN Member?</p>
+    <div class="flex gap-[20px]">
+        <div>
+            <input type="radio" id="member_regular" name="membership" value="regular">
+            <label for="member_regular">Regular</label>
+        </div>
+        <div>
+            <input type="radio" id="member_associate" name="membership" value="associate">
+            <label for="member_associate">Associate</label>
+        </div>
+        <div>
+            <input type="radio" id="member_donor" name="membership" value="Donor">
+            <label for="member_donor">Donor</label>
+        </div>
+        <div>
+            <input type="radio" id="non_member" name="membership" value="non_member" required>
+            <label for="non_member">Not a member</label>
+        </div>
+    </div>
+</div>
+
+<!-- Souvenir Program -->
+<div class="flex flex-col">
+    <p class="sub-bi-heading text-[#344054]">Souvenir Program</p>
+    <div class="flex gap-[20px]">
+        <div>
+            <input type="radio" id="sv_printed" name="souvenir" value="printed" required>
+            <label for="sv_printed">Printed (First 400 only)</label>
+        </div>
+        <div>
+            <input type="radio" id="sv_digital" name="souvenir" value="digital">
+            <label for="sv_digital">Digital</label>
+        </div>
+    </div>
+</div>
+
+<!-- Certificate of Attendance -->
+<div class="flex flex-col">
+    <p class="sub-bi-heading text-[#344054]">Do you need a Certificate of Attendance?</p>
+    <div class="flex gap-[20px]">
+        <div>
+            <input type="radio" id="cert_yes" name="certificate_needed" value="yes" required>
+            <label for="cert_yes">Yes</label>
+        </div>
+        <div>
+            <input type="radio" id="cert_no" name="certificate_needed" value="no">
+            <label for="cert_no">No</label>
+        </div>
+    </div>
+</div>
+
+<!-- Sponsored Registration -->
+<!-- <div class="flex flex-col">
+    <p class="sub-bi-heading text-[#344054]">Sponsored Registration?</p>
+    <div class="flex gap-[20px]">
+        <div>
+            <input type="radio" id="sponsored_yes" name="sponsored" value="yes" required>
+            <label for="sponsored_yes">Yes</label>
+        </div>
+        <div>
+            <input type="radio" id="sponsored_no" name="sponsored" value="no">
+            <label for="sponsored_no">No</label>
+        </div>
+    </div>
+</div> -->
---
+++ 2025-anual-event-template/fields/personal-info-fields.php
@@ -0,0 +1,91 @@
+<div class="w-[100%] flex flex-col gap-[10px]">
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Your email</p>
+        <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            readonly
+            placeholder="Email address"
+            class="w-full p-3 border bg-[beige] text-[#a9a9a9] text-[bolder] italic border-[2px] border-[#9e9e47]"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">First Name</p>
+        <input
+            id="first_name"
+            name="first_name"
+            type="text"
+            required
+            placeholder="First Name"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Middle Name</p>
+        <input
+            id="middle_name"
+            name="middle_name"
+            type="text"
+            required
+            placeholder="Middle Name"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Last Name</p>
+        <input
+            id="last_name"
+            name="last_name"
+            type="text"
+            required
+            placeholder="Last Name"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Mobile Number</p>
+        <input
+            id="mobile"
+            name="mobile"
+            type="text"
+            required
+            placeholder="Mobile NUmber"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Company</p>
+        <input
+            id="company"
+            name="company"
+            type="text"
+            required
+            placeholder="Company"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Position Title</p>
+        <input
+            id="position"
+            name="position"
+            type="text"
+            required
+            placeholder="Position/Title"
+            class="w-full p-3 border"
+        />
+    </div>
+    <div class="flex flex-col">
+        <p class="sub-bi-heading text-[#344054]">Agricultural License Number</p>
+        <input
+            id="agri_license"
+            name="agri_license"
+            type="text"
+            required
+            placeholder="Agricultural License Number"
+            class="w-full p-3 border"
+        />
+    </div>
+</div>
