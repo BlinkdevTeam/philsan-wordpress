131e7a38844dba613f1775ba200f24ebb9c11159
KenMirwell <kenmirwell@gmail.com>
2025-06-10 10:33:10 +0800

    update

--- 2025-anual-event-template/fields/sponsors-fields.php
+++ 2025-anual-event-template/fields/sponsors-fields.php
@@ -22,9 +22,96 @@
             <label for="camden">Camden</label>
         </div>
         <div>
-            <input type="radio" id="" name="sponsor" value="E Company">
-            <label for=""></label>
+            <input type="radio" id="docinex" name="sponsor" value="E Company">
+            <label for="docinex">Diconex</label>
         </div>
-
+        <div>
+            <input type="radio" id="dsm" name="sponsor" value="E Company">
+            <label for="dsm">DSM</label>
+        </div>
+        <div>
+            <input type="radio" id="east-asia" name="sponsor" value="E Company">
+            <label for="east-asia">East Asia</label>
+        </div>
+        <div>
+            <input type="radio" id="easybio" name="sponsor" value="E Company">
+            <label for="easybio">Easybio</label>
+        </div>
+        <div>
+            <input type="radio" id="equilivet" name="sponsor" value="E Company">
+            <label for="equilivet">Equilivet</label>
+        </div>
+        <div>
+            <input type="radio" id="evonik" name="sponsor" value="E Company">
+            <label for="evonik">Evonik</label>
+        </div>
+        <div>
+            <input type="radio" id="Inphilco" name="sponsor" value="E Company">
+            <label for="Inphilco">inphilco</label>
+        </div>
+        <div>
+            <input type="radio" id="Interfarma" name="sponsor" value="E Company">
+            <label for="Interfarma">interfarma</label>
+        </div>
+        <div>
+            <input type="radio" id="jcs" name="sponsor" value="E Company">
+            <label for="jcs">JCS</label>
+        </div>
+        <div>
+            <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="E Company">
+            <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
+        </div>
+        <div>
+            <input type="radio" id="kpp-powers" name="sponsor" value="E Company">
+            <label for="kpp-powers">KPP Powers</label>
+        </div>
+        <div>
+            <input type="radio" id="maxion" name="sponsor" value="E Company">
+            <label for="maxion">Maxion</label>
+        </div>
+        <div>
+            <input type="radio" id="mgb" name="sponsor" value="E Company">
+            <label for="mgb">MGB</label>
+        </div>
+        <div>
+            <input type="radio" id="nuevo-milenio" name="sponsor" value="E Company">
+            <label for="nuevo-milenio">Nuevo Milenio</label>
+        </div>
+        <div>
+            <input type="radio" id="novus" name="sponsor" value="E Company">
+            <label for="novus">Novus</label>
+        </div>
+        <div>
+            <input type="radio" id="philcema" name="sponsor" value="E Company">
+            <label for="philcema">Philcema</label>
+        </div>
+        <div>
+            <input type="radio" id="promax" name="sponsor" value="E Company">
+            <label for="promax">Promax</label>
+        </div>
+        <div>
+            <input type="radio" id="realvet" name="sponsor" value="E Company">
+            <label for="realvet">Realvet</label>
+        </div>
+        <div>
+            <input type="radio" id="rnt" name="sponsor" value="E Company">
+            <label for="rnt">RNT</label>
+        </div>
+        <div>
+            <input type="radio" id="seastemas" name="sponsor" value="E Company">
+            <label for="seastemas">Seastemas</label>
+        </div>
+        <div>
+            <input type="radio" id="st-john-biomedics" name="sponsor" value="E Company">
+            <label for="st-john-biomedics">St. John Biomedics</label>
+        </div>
+        <div>
+            <input type="radio" id="vet-health" name="sponsor" value="E Company">
+            <label for="vet-health">Vet Health</label>
+        </div>
+        <div>
+            <input type="radio" id="vet-specialist" name="sponsor" value="E Company">
+            <label for="vet-specialist">Vet Specialist</label>
+        </div>
     </div>
 </div>
