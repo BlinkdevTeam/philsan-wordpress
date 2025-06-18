eb628b245952199e472bf9a7dd20f6b53b8bf89e
KenMirwell <kenmirwell@gmail.com>
2025-06-18 15:22:14 +0800

    update

--- 2025-anual-event-template/fields/personal-info-fields.php
+++ 2025-anual-event-template/fields/personal-info-fields.php
@@ -11,49 +11,53 @@
             class="w-full p-3 border bg-[beige] text-[#a9a9a9] text-[bolder] italic border-[2px] border-[#9e9e47]"
         />
     </div>
-    <div class="flex flex-col">
-        <p class="sub-bi-heading text-[#344054]">First Name</p>
-        <input
-            id="first_name"
-            name="first_name"
-            type="text"
-            required
-            placeholder="First Name"
-            class="w-full p-3 border"
-        />
+    <div class="flex justify-between gap-[20px]">
+        <div class="flex flex-col">
+            <p class="sub-bi-heading text-[#344054]">First Name</p>
+            <input
+                id="first_name"
+                name="first_name"
+                type="text"
+                required
+                placeholder="First Name"
+                class="w-full p-3 border"
+            />
+        </div>
+        <div class="flex flex-col">
+            <p class="sub-bi-heading text-[#344054]">Middle Name</p>
+            <input
+                id="middle_name"
+                name="middle_name"
+                type="text"
+                required
+                placeholder="Middle Name"
+                class="w-full p-3 border"
+            />
+        </div>
     </div>
-    <div class="flex flex-col">
-        <p class="sub-bi-heading text-[#344054]">Middle Name</p>
-        <input
-            id="middle_name"
-            name="middle_name"
-            type="text"
-            required
-            placeholder="Middle Name"
-            class="w-full p-3 border"
-        />
-    </div>
-    <div class="flex flex-col">
-        <p class="sub-bi-heading text-[#344054]">Last Name</p>
-        <input
-            id="last_name"
-            name="last_name"
-            type="text"
-            required
-            placeholder="Last Name"
-            class="w-full p-3 border"
-        />
-    </div>
-    <div class="flex flex-col">
-        <p class="sub-bi-heading text-[#344054]">Mobile Number</p>
-        <input
-            id="mobile"
-            name="mobile"
-            type="text"
-            required
-            placeholder="Mobile NUmber"
-            class="w-full p-3 border"
-        />
+    <div class="flex justify-between gap-[20px]">
+        <div class="flex flex-col">
+            <p class="sub-bi-heading text-[#344054]">Last Name</p>
+            <input
+                id="last_name"
+                name="last_name"
+                type="text"
+                required
+                placeholder="Last Name"
+                class="w-full p-3 border"
+            />
+        </div>
+        <div class="flex flex-col">
+            <p class="sub-bi-heading text-[#344054]">Mobile Number</p>
+            <input
+                id="mobile"
+                name="mobile"
+                type="text"
+                required
+                placeholder="Mobile NUmber"
+                class="w-full p-3 border"
+            />
+        </div>
     </div>
     <div class="flex flex-col">
         <p class="sub-bi-heading text-[#344054]">Company</p>
--- 2025-anual-event-template/fields/sponsors-fields.php
+++ 2025-anual-event-template/fields/sponsors-fields.php
@@ -1,125 +1,5 @@
-<!-- <div class="flex flex-col">
-    <p class="sub-bi-heading text-[#344054]">Who's your sponsor?</p>
-    <div class="flex flex-col gap-[10px]">
-        <div>
-            <input type="radio" id="no-sponsor" name="sponsor" value="A Company" required>
-            <label for="no-sponsor">No Sponsor</label>
-        </div>
-        <div>
-            <input type="radio" id="alltech" name="sponsor" value="A Company" required>
-            <label for="alltech">Alltech</label>
-        </div>
-        <div>
-            <input type="radio" id="ariela" name="sponsor" value="B Company">
-            <label for="ariela">Ariela</label>
-        </div>
-        <div>
-            <input type="radio" id="awap" name="sponsor" value="C Company">
-            <label for="awap">AWAP</label>
-        </div>
-        <div>
-            <input type="radio" id="bounty_plus" name="sponsor" value="D Company">
-            <label for="bounty_plus">Bounty Plus</label>
-        </div>
-        <div>
-            <input type="radio" id="camden" name="sponsor" value="E Company">
-            <label for="camden">Camden</label>
-        </div>
-        <div>
-            <input type="radio" id="docinex" name="sponsor" value="E Company">
-            <label for="docinex">Diconex</label>
-        </div>
-        <div>
-            <input type="radio" id="dsm" name="sponsor" value="E Company">
-            <label for="dsm">DSM</label>
-        </div>
-        <div>
-            <input type="radio" id="east-asia" name="sponsor" value="E Company">
-            <label for="east-asia">East Asia</label>
-        </div>
-        <div>
-            <input type="radio" id="easybio" name="sponsor" value="E Company">
-            <label for="easybio">Easybio</label>
-        </div>
-        <div>
-            <input type="radio" id="equilivet" name="sponsor" value="E Company">
-            <label for="equilivet">Equilivet</label>
-        </div>
-        <div>
-            <input type="radio" id="evonik" name="sponsor" value="E Company">
-            <label for="evonik">Evonik</label>
-        </div>
-        <div>
-            <input type="radio" id="Inphilco" name="sponsor" value="E Company">
-            <label for="Inphilco">inphilco</label>
-        </div>
-        <div>
-            <input type="radio" id="Interfarma" name="sponsor" value="E Company">
-            <label for="Interfarma">interfarma</label>
-        </div>
-        <div>
-            <input type="radio" id="jcs" name="sponsor" value="E Company">
-            <label for="jcs">JCS</label>
-        </div>
-        <div>
-            <input type="radio" id="jebsen-adn-jessen" name="sponsor" value="E Company">
-            <label for="jebsen-adn-jessen">Jebsen and Jessen</label>
-        </div>
-        <div>
-            <input type="radio" id="kpp-powers" name="sponsor" value="E Company">
-            <label for="kpp-powers">KPP Powers</label>
-        </div>
-        <div>
-            <input type="radio" id="maxion" name="sponsor" value="E Company">
-            <label for="maxion">Maxion</label>
-        </div>
-        <div>
-            <input type="radio" id="mgb" name="sponsor" value="E Company">
-            <label for="mgb">MGB</label>
-        </div>
-        <div>
-            <input type="radio" id="nuevo-milenio" name="sponsor" value="E Company">
-            <label for="nuevo-milenio">Nuevo Milenio</label>
-        </div>
-        <div>
-            <input type="radio" id="novus" name="sponsor" value="E Company">
-            <label for="novus">Novus</label>
-        </div>
-        <div>
-            <input type="radio" id="philcema" name="sponsor" value="E Company">
-            <label for="philcema">Philcema</label>
-        </div>
-        <div>
-            <input type="radio" id="promax" name="sponsor" value="E Company">
-            <label for="promax">Promax</label>
-        </div>
-        <div>
-            <input type="radio" id="realvet" name="sponsor" value="E Company">
-            <label for="realvet">Realvet</label>
-        </div>
-        <div>
-            <input type="radio" id="rnt" name="sponsor" value="E Company">
-            <label for="rnt">RNT</label>
-        </div>
-        <div>
-            <input type="radio" id="seastemas" name="sponsor" value="E Company">
-            <label for="seastemas">Seastemas</label>
-        </div>
-        <div>
-            <input type="radio" id="st-john-biomedics" name="sponsor" value="E Company">
-            <label for="st-john-biomedics">St. John Biomedics</label>
-        </div>
-        <div>
-            <input type="radio" id="vet-health" name="sponsor" value="E Company">
-            <label for="vet-health">Vet Health</label>
-        </div>
-        <div>
-            <input type="radio" id="vet-specialist" name="sponsor" value="E Company">
-            <label for="vet-specialist">Vet Specialist</label>
-        </div>
-    </div>
-</div> -->
-    <div class="flex flex-col">
+<!-- 
+<div class="flex flex-col">
     <p class="sub-bi-heading text-[#344054] mb-4">Who's your sponsor?</p>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-3 gap-x-6">
@@ -241,5 +121,42 @@
         </div>
 
     </div>
-    </div>
+</div>
+ -->
 
+
+<div class="flex flex-col">
+  <label for="sponsor" class="sub-bi-heading text-[#344054] mb-4">Who's your sponsor?</label>
+  
+  <select name="sponsor" id="sponsor" required class="border border-gray-300 rounded-md p-2 w-full max-w-md">
+    <option value="No Sponsor" selected>No Sponsor</option>
+    <option value="Alltech">Alltech</option>
+    <option value="Ariela">Ariela</option>
+    <option value="AWAP">AWAP</option>
+    <option value="Bounty Plus">Bounty Plus</option>
+    <option value="Camden">Camden</option>
+    <option value="Docinex">Docinex</option>
+    <option value="DSM">DSM</option>
+    <option value="East Asia">East Asia</option>
+    <option value="Easybio">Easybio</option>
+    <option value="Equilivet">Equilivet</option>
+    <option value="Evonik">Evonik</option>
+    <option value="Inphilco">Inphilco</option>
+    <option value="Interfarma">Interfarma</option>
+    <option value="JCS">JCS</option>
+    <option value="Jebsen and Jessen">Jebsen and Jessen</option>
+    <option value="KPP Powers">KPP Powers</option>
+    <option value="Maxion">Maxion</option>
+    <option value="MGB">MGB</option>
+    <option value="Nuevo Milenio">Nuevo Milenio</option>
+    <option value="Novus">Novus</option>
+    <option value="Philcema">Philcema</option>
+    <option value="Promax">Promax</option>
+    <option value="Realvet">Realvet</option>
+    <option value="RNT">RNT</option>
+    <option value="Seastemas">Seastemas</option>
+    <option value="St. John Biomedics">St. John Biomedics</option>
+    <option value="Vet Health">Vet Health</option>
+    <option value="Vet Specialist">Vet Specialist</option>
+  </select>
+</div>
