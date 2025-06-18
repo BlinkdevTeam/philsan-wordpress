3842cb878fa97d3d865331153737f4b6ed59d175
KenMirwell <kenmirwell@gmail.com>
2025-06-18 12:18:18 +0800

    update

--- 2025-anual-event-template/template-email-verification.php
+++ 2025-anual-event-template/template-email-verification.php
@@ -12,14 +12,14 @@ get_header();
 ?>
 
 <div class="bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] bg-cover bg-center h-[100vh] overflow-hidden w-full relative">
-  <div class="mx-auto w-[1280px] h-[100vh] flex items-center">
+  <div class="mx-auto w-[90%] xl:w-[1280px] h-[100vh] flex items-center">
       <div class="flex w-[100%] h-max mt-[-150px]">
           <div class="w-[50%] px-[50px] flex flex-col gap-[20px] justify-center items-center text-center">
               <img width="60%" src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="mx-auto" />
               <div class="flex flex-col justify-center items-center text-center gap-[20px]">
                   <p class="text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
                   <p class="text-[38px] font-bold text-[#1F773A] font-fraunces">September 30, 2025</p>
-                  <div class="flex flex-col items-center bg-gradient-to-r from-[#1F773A] to-[#EDB221] w-max px-[50px] py-[10px] rounded-tl-[40px] rounded-br-[40px]">
+                  <div class="flex flex-col items-center bg-gradient-to-r from-[#1F773A] to-[#EDB221] w-max px-[90px] py-[10px] rounded-tl-[40px] rounded-br-[40px]">
                     <p class="text-[22px] text-[#ffffff] font-fraunces">Okada Manila Paranaque City,</p>
                     <p class="text-[22px] text-[#ffffff] font-fraunces">Philippines</p>
                   </div>
