7687583271c9297dde44502f137321acb858576a
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-06-19 21:19:14 +0800

    update

--- 2025-anual-event-template/components/convention-details.php
+++ 2025-anual-event-template/components/convention-details.php
@@ -1,14 +1,14 @@
- <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
-  <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
-      <div class="flex w-[100%] h-max">
-        <div class="w-[50%] px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
-            <img width="60%" src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="mx-auto" />
-            <div class="flex flex-col justify-center items-center text-center gap-[20px]">
-                <p class="text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
-                <p class="text-[38px] font-bold text-[#1F773A] font-fraunces">September 30, 2025</p>
-                <div class="flex flex-col items-center bg-gradient-to-r from-[#1F773A] to-[#EDB221] w-max px-[90px] py-[10px] rounded-tl-[40px] rounded-br-[40px]">
-                    <p class="text-[22px] text-[#ffffff] font-fraunces">Okada Manila Paranaque City,</p>
-                    <p class="text-[22px] text-[#ffffff] font-fraunces">Philippines</p>
+<div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
+    <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
+        <div class="flex w-[100%] h-max">
+            <div class="w-[50%] px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
+                <img src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="w-[60%] mx-auto" />
+                <div class="flex flex-col justify-center items-center text-center gap-[20px]">
+                    <p class="text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
+                    <p class="text-[38px] font-bold text-[#1F773A] font-fraunces">September 30, 2025</p>
+                    <div class="flex flex-col items-center bg-gradient-to-r from-[#1F773A] to-[#EDB221] w-max px-[90px] py-[10px] rounded-tl-[40px] rounded-br-[40px]">
+                        <p class="text-[22px] text-[#ffffff] font-fraunces">Okada Manila Paranaque City,</p>
+                        <p class="text-[22px] text-[#ffffff] font-fraunces">Philippines</p>
+                    </div>
                 </div>
             </div>
-        </div>
--- 2025-anual-event-template/template-visit-email.php
+++ 2025-anual-event-template/template-visit-email.php
@@ -11,7 +11,7 @@ get_header();
 <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
           <div class="flex w-[50%]">
             <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center px-[50px]">
-                <div class="relative overflow-hidden pb-[125px] pt-[50px] w-auto h-auto flex flex-col justify-center px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
+                <div class="relative overflow-hidden pb-[125px] lg:pb-[160px] pt-[50px] w-auto h-auto flex flex-col justify-center px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
                     <div class="pb-[30px]">
                         <img src="https://philsan.org/wp-content/uploads/2025/06/Sent-Philsan-1.png" alt="">
                     </div>  
