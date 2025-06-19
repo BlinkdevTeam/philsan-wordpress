ba3666683bc07425aead6e4f7e3af8e6999a6a16
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-06-19 21:33:52 +0800

    update

--- 2025-anual-event-template/template-email-verification.php
+++ 2025-anual-event-template/template-email-verification.php
@@ -11,9 +11,11 @@ get_header();
     the_post();
 ?>
 
-
-<?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
-          <div class="flex w-[50%]">
+<div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
+    <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
+        <div class="flex w-[100%] h-max">
+          <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
+          <div class="flex flex w-[80%] lg:w-[50%]">
               <form id="email-verification" class="w-[100%] text-black flex flex-col justify-center px-[50px]">
                   <div class="relative overflow-hidden pb-[125px] pb-[125px] lg:pb-[160px] pt-[50px] px-[50px] w-auto h-auto flex flex-col justify-center rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
                     <h2 class="text-[38px] text-[#1F773A] font-fraunces font-bold">Register</h2>
--- 2025-anual-event-template/template-registration-successful.php
+++ 2025-anual-event-template/template-registration-successful.php
@@ -12,7 +12,7 @@ get_header();
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max">
             <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
-            <div class="flex w-[50%]">
+            <div class="flex w-[80%] lg:w-[50%]">
                 <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center px-[50px]">
                     <div class="relative overflow-hidden pb-[125px] lg:pb-[160px] pt-[50px] w-auto flex flex-col gap-[50px] justify-start h-auto px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
                         <?php get_template_part('2025-anual-event-template/components/mobile-convention-details'); ?>  
