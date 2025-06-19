5d1a27cb3a041e5557a43900b21904824b97b544
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-06-19 23:53:50 +0800

    update

--- 2025-anual-event-template/components/mobile-convention-details.php
+++ 2025-anual-event-template/components/mobile-convention-details.php
@@ -1,7 +1,7 @@
 <!-- <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max"> -->
-            <div class="w-[100%] lg:hidden px-[50px] flex flex-col gap-[20px] justify-start items-center text-center pb-[100px]">
+            <div class="w-[100%] lg:hidden px-[0px] md:px-[50px] flex flex-col gap-[20px] justify-start items-center text-center pb-[100px]">
                 <img src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="w-[45%] lg:w-[60%] mx-auto" />
                 <div class="flex flex-col justify-center items-center text-center gap-[20px]">
                     <p class="text-[16px] lg:text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
