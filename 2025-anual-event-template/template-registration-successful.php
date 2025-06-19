589ead5ee14bbaa22937086225ad2afef2ce0de3
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-06-19 21:50:58 +0800

    update

--- 2025-anual-event-template/template-registration-successful.php
+++ 2025-anual-event-template/template-registration-successful.php
@@ -10,7 +10,7 @@ get_header();
 
 <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
-        <div class="flex w-[100%] h-max">
+        <div class="flex w-[100%] h-max justify-center">
             <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
             <div class="flex w-[80%] lg:w-[50%]">
                 <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center px-[50px]">
