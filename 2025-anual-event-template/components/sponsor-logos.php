9547ff70ef61f8a8dd7ee6ec979378a92130174f
KenMirwell <kenmirwell@gmail.com>
2025-06-20 16:38:16 +0800

    update

--- 2025-anual-event-template/components/sponsor-logos.php
+++ 2025-anual-event-template/components/sponsor-logos.php
@@ -1,24 +1,34 @@
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
         <div class="swiper-slide p-[20px] shadow-lg">
-            <p>Diamond</p>
+            <div class="bg-[#1F773A] p-[10px]">
+                <p class="font-fraunces font-bold text-[#ffffff]">Diamond</p>
+            </div>
             <img class="w-[50%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226337.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] shadow-lg">
-            <p>Gold</p>
+            <div class="bg-[#1F773A] p-[10px]">
+                <p class="font-fraunces font-bold text-[#ffffff]">Gold</p>
+            </div>
             <img class="w-[50%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226338.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] shadow-lg">
-            <p>3rd Level</p>
-            <img class="w-[100%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226341-scaled.png" alt="">
+            <div class="bg-[#1F773A] p-[10px]">
+                <p class="font-fraunces font-bold text-[#ffffff]">3rd Level</p>
+            </div>
+            <img class="w-[80%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226341-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] shadow-lg">
-            <p>2nd Level</p>
-            <img class="w-[100%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226340-1-scaled.png" alt="">
+            <div class="bg-[#1F773A] p-[10px]">
+                <p class="font-fraunces font-bold text-[#ffffff]">2nd Level</p>
+            </div>
+            <img class="w-[80%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226340-1-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] shadow-sm">
-            <p>Platinum</p>
-            <img class="w-[100%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226339-scaled.png" alt="">
+            <div class="bg-[#1F773A] p-[10px]">
+                <p class="font-fraunces font-bold text-[#ffffff]">Platinum</p>
+            </div>
+            <img class="w-[80%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226339-scaled.png" alt="">
         </div>
     </div>
     <div class="swiper-pagination"></div>
