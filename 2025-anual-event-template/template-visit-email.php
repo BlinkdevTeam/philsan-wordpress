79882a2defa3a0b9981ef401a16e975d89de70ec
KenMirwell <kenmirwell@gmail.com>
2025-06-20 15:58:52 +0800

    update

--- 2025-anual-event-template/template-visit-email.php
+++ 2025-anual-event-template/template-visit-email.php
@@ -11,9 +11,9 @@ get_header();
 <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max justify-center">
+            <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
             <div class="relative flex flex-col gap-[10px] justify-center">
-                <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
-                
+                <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
             </div>
             <div class="flex flex w-[100%] lg:w-[50%]">
                 <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center lg:px-[50px]">
