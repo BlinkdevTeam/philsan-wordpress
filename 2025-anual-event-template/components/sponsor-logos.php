8a9a552bc47e69eb202e0997109a437f690dad34
KenMirwell <kenmirwell@gmail.com>
2025-06-20 15:52:37 +0800

    update

--- 2025-anual-event-template/components/sponsor-logos.php
+++ 2025-anual-event-template/components/sponsor-logos.php
@@ -1,28 +1,31 @@
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
-        <div class="swiper-slide">
+        <div class="swiper-slide p-[20px] shadow-lg">
             <p>Diamond</p>
             <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226337.png" alt="">
         </div>
-        <div class="swiper-slide">
+        <div class="swiper-slide p-[20px] shadow-lg">
             <p>Gold</p>
             <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226338.png" alt="">
         </div>
-        <div class="swiper-slide">
+        <div class="swiper-slide p-[20px] shadow-lg">
             <p>3rd Level</p>
             <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226341-scaled.png" alt="">
         </div>
-        <div class="swiper-slide">
+        <div class="swiper-slide p-[20px] shadow-lg">
             <p>2nd Level</p>
             <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226340-1-scaled.png" alt="">
         </div>
-        <div class="swiper-slide">
+        <div class="swiper-slide p-[20px] shadow-lg">
             <p>Platinum</p>
             <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226339-scaled.png" alt="">
         </div>
     </div>
     <div class="swiper-pagination"></div>
   </div>
+
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
   <!-- Initialize Swiper -->
 <script>
--- 2025-anual-event-template/template-visit-email.php
+++ 2025-anual-event-template/template-visit-email.php
@@ -11,8 +11,10 @@ get_header();
 <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max justify-center">
-            <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
-            <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
+            <div class="flex flex-col gap-[10px] justify-center">
+                <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
+                <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
+            </div>
             <div class="flex flex w-[100%] lg:w-[50%]">
                 <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center lg:px-[50px]">
                     <div class="relative overflow-hidden pb-[250px] lg:pb-[160px] pt-[50px] w-auto h-auto flex flex-col justify-center px-[20px] md:px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
