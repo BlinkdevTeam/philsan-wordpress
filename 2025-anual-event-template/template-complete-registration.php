8c0ee492e9091997366d0f65f325f31baaa4cc1e
KenMirwell <kenmirwell@gmail.com>
2025-06-23 13:55:50 +0800

    update

--- 2025-anual-event-template/template-complete-registration.php
+++ 2025-anual-event-template/template-complete-registration.php
@@ -24,14 +24,14 @@ get_header();
                         </div>
                         <div class="flex flex-col lg:flex-row justify-between gap-[40px] pt-[20px]">
 
-                            <div>
+                            <div class="w-[50%]">
                                 <?php get_template_part('2025-anual-event-template/fields/personal-info-fields'); ?>  
 
                                 <?php get_template_part('2025-anual-event-template/fields/other-checkboxes'); ?>  
                             </div>
                             
                             <!-- checkbox section -->
-                            <div class="w-[100%] flex flex-col gap-[15px]">
+                            <div class="w-[50%] flex flex-col gap-[15px]">
 
                                 <?php 
                                 // get_template_part('2025-anual-event-template/fields/other-checkboxes'); 
