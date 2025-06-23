b625d628339f3fc7255e268a9981f8ea9d3e5687
KenMirwell <kenmirwell@gmail.com>
2025-06-23 11:01:27 +0800

    update

--- 2025-anual-event-template/components/convention-details.php
+++ 2025-anual-event-template/components/convention-details.php
@@ -1,8 +1,8 @@
 <!-- <div class="registration-middle-content bg-[url('https://philsan.org/wp-content/uploads/2025/06/17580-1-scaled.png')] justify-center min-h-screen bg-center w-full relative flex flex-col gap-[20px]">
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max"> -->
-            <div class="w-[50%] flex flex-col gap-[10px] justify-center">
-                <div class="w-[100%] hidden lg:block px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
+            <div class="w-[50%] hidden lg:block px-[50px] flex flex-col gap-[10px] justify-center">
+                <div class="w-[100%] px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
                     <img src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="w-[60%] mx-auto" />
                     <div class="flex flex-col justify-center items-center text-center gap-[20px] pt-[20px]">
                         <p class="text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technologies and Alternative Solutions in Animal Nutrition and Health</p>
@@ -13,7 +13,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="relative hidden lg:block px-[50px]">
+                <div class="relative">
                     <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
                 </div>
             </div>
