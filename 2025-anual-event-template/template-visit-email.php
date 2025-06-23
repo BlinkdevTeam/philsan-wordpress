23518f4d006e45a96418570dfb1435a090c1a4ac
KenMirwell <kenmirwell@gmail.com>
2025-06-23 10:52:46 +0800

    update

--- 2025-anual-event-template/template-visit-email.php
+++ 2025-anual-event-template/template-visit-email.php
@@ -12,7 +12,7 @@ get_header();
     <div class="mx-auto w-[90%] xl:w-[1280px] flex items-center py-[60px]">
         <div class="flex w-[100%] h-max justify-center">
             <div class="w-[50%] flex flex-col gap-[10px] justify-center">
-                <!-- <div class="w-[100%] hidden lg:block px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
+                <div class="w-[100%] hidden lg:block px-[50px] flex flex-col gap-[20px] justify-start items-center text-center">
                     <img src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="Logo" class="w-[60%] mx-auto" />
                     <div class="flex flex-col justify-center items-center text-center gap-[20px] pt-[20px]">
                         <p class="text-[22px] text-center font-poppins font-bold">Innovating for a Sustainable Future: Harnessing Technologies and Alternative Solutions in Animal Nutrition and Health</p>
@@ -24,12 +24,9 @@ get_header();
                     </div>
                 </div>
                 <div class="relative">
-                    <?php 
-                        // get_template_part('2025-anual-event-template/components/sponsor-logos'); 
-                    ?>  
-                </div> -->
+                    <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
+                </div>
             </div>
-             <?php get_template_part('2025-anual-event-template/components/convention-details'); ?>  
             <div class="flex flex w-[100%] lg:w-[50%]">
                 <div id="email-verification" class="w-[100%] text-black flex flex-col justify-center lg:px-[50px]">
                     <div class="relative overflow-hidden pb-[250px] lg:pb-[160px] pt-[50px] w-auto h-auto flex flex-col justify-center px-[20px] md:px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
