9d2e47b863c86fdc77b50bb6c2ba77dfa5b88979
KenMirwell <kenmirwell@gmail.com>
2025-06-23 11:06:51 +0800

    update

--- 2025-anual-event-template/template-registration-successful.php
+++ 2025-anual-event-template/template-registration-successful.php
@@ -17,13 +17,16 @@ get_header();
                     <div class="relative overflow-hidden pb-[200px] lg:pb-[160px] pt-[50px] w-auto flex flex-col gap-[50px] justify-start h-auto px-[20px] md:px-[50px] py-[50px] rounded text-start bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] shadow-lg rounded-lg">
                         <?php get_template_part('2025-anual-event-template/components/mobile-convention-details'); ?>  
                         <div class="flex flex-col justify-center items-center gap-[30px]">
-                            <div class="w-[150px]">
+                            <div class="w-[100px] lg:w-[150px]">
                                 <img src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Icon-1.png" alt="">
                             </div>  
-                            <p class="text-[22px] font-poppins"><strong class="text-[#1F773A] font-bold">PHILSAN will review your registration.</strong> Once approved, you will receive a confirmation email along with your QR code.</p>
+                            <p class="text-[22px] text-center lg:text-[left] font-poppins"><strong class="text-[#1F773A] font-bold">PHILSAN will review your registration.</strong> Once approved, you will receive a confirmation email along with your QR code.</p>
                         </div>
                         <div class="flex gap-[20px] items-center z-[1] justify-center">
                             <a href="https://philsan.org/38th-annual-convention/register/" class="flex items-center py-[10px] px-[40px] w-max h-[60px] bg-[#1F773A] hover:bg-[#EDB221] text-[#ffffff] cursor-pointer rounded-tl-[30px] rounded-br-[30px] font-fraunces">Start New Registration</a>
+                        </div>
+                        <div class="relative lg:hidden">
+                            <?php get_template_part('2025-anual-event-template/components/sponsor-logos'); ?>  
                         </div>
                         <?php get_template_part('2025-anual-event-template/components/poster-image'); ?>  
                     </div>
