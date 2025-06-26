f6be114ebece39ef2170420e8e123ebf833c8fa2
KenMirwell <kenmirwell@gmail.com>
2025-06-26 12:22:09 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -7,21 +7,21 @@ while (have_posts()) {
 the_post();
 ?>
 
-<div class="hero-section bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)]">
+<div class="hero-section bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] overflow-hidden">
     <div class="flex gap-[50px] w-[1280px] mx-auto">
         <div class="flex flex-col w-[50%] gap-[20px]">
             <img class="w-[100px]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226336.png" alt="">
             <img class="w-[80%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226387-1.png" alt="">
             <p class="font-bold text-[#000000]">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
             <div class="flex flex-col gap-[10px]">
-                <p class="font-bold text-[#1F773A]">SEPTEMBER 30, 2025</p>
-                <p class="font-bold text-[#EDB221]">Okada Manila, Paranaque City, Philippines</p>
+                <p class="font-bold text-[#1F773A] text-[28px]">SEPTEMBER 30, 2025</p>
+                <p class="font-bold text-[#EDB221] text-[18px]">Okada Manila, Paranaque City, Philippines</p>
             </div>
-            <a class="bg-gradient-to-r from-[#1F773A] to-[#EDB221]" href="">Register</a>
+            <a class="w-max bg-gradient-to-r from-[#1F773A] to-[#EDB221] text-[#ffffff] text-[28px] py-[20px] px-[50px] rounded-tl-[80px] rounded-br-[80px]" href="">Register</a>
         </div>
         <div class="relative w-[50%]">
             <img class="z-[2]" src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
-            <img class="absolute z-[1] bottom-[-30px] w-[110%] opacity-[0.4]" src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
+            <img class="absolute z-[1] bottom-[-30px] transform scale-110 opacity-[0.2]" src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
         </div>
     </div>
 </div>
