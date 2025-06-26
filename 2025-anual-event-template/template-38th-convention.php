325fc0e635fa9fa2d52fac1e65f80ded51e8da57
KenMirwell <kenmirwell@gmail.com>
2025-06-26 11:45:26 +0800

    update

---
+++ 2025-anual-event-template/template-38th-convention.php
@@ -0,0 +1,15 @@
+<?php 
+ /* Template Name: 38th Convention */
+?>
+
+<div class="hero-section bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)]">
+    <div class="w-[1280px]">
+        <div class="w-[50%]">
+            <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226336.png" alt="">
+            <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226387.png" alt="">
+        </div>
+        <div class="w-[50%]">
+            <img src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
+        </div>
+    </div>
+</div>
