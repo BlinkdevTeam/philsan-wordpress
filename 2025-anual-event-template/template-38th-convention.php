5247c7e7c549e8ca23ec2000d3aee23224396a1a
KenMirwell <kenmirwell@gmail.com>
2025-06-26 12:04:35 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -3,10 +3,10 @@
 ?>
 
 <div class="hero-section bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)]">
-    <div class="w-[1280px]">
-        <div class="w-[50%]">
-            <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226336.png" alt="">
-            <img src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226387.png" alt="">
+    <div class="flex gap-[50px] w-[1280px] mx-auto">
+        <div class="flex flex-col w-[50%]">
+            <img class="w-[100px]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226336.png" alt="">
+            <img class="w-[80%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226387-1.png" alt="">
         </div>
         <div class="w-[50%]">
             <img src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
