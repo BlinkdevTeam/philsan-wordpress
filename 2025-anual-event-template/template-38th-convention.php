376ef83c94b41accf79dae90e8cbb6c8fe388fb0
KenMirwell <kenmirwell@gmail.com>
2025-06-30 12:41:53 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -174,7 +174,7 @@ while (have_posts()) {
                                                     <img class="w-full h-full object-cover" src="<?php echo esc_url(get_sub_field('speaker_image')); ?>" alt="<?php the_title(); ?>">
                                                 </div>
                                             </div>
-                                            <div class="w-[400px] flex flex-col items-center rounded-br-xl justify-center gap-[10px] py-[20px] px-[20px] bg-[#1F773A] text-white">
+                                            <div class="w-[400px] flex flex-col items-center rounded-br-[50px] justify-center gap-[10px] py-[20px] px-[20px] bg-[#1F773A] text-white">
                                                 <p class="text-center text-[22px] font-bold"><?php echo esc_html(get_sub_field('speaker_name')); ?></p>
                                                 <div class="h-[50px] w-[340px]">
                                                     <p class="text-center font-[300]"><?php echo esc_html(get_sub_field('speaker_title')); ?></p>
