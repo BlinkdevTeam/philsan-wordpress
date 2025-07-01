1db532068bcca28aaddb6e3c3e20908dba9f15e2
KenMirwell <kenmirwell@gmail.com>
2025-07-01 14:17:01 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -166,19 +166,29 @@ while (have_posts()) {
                             <div class="flex justify-center gap-[40px] flex-wrap pt-[50px]">
                                 <?php if (have_rows('speaker')) : ?>
                                     <?php while (have_rows('speaker')) : the_row(); ?>
-                                        <div class="relative flex flex-row md:flex-col items-end md:items-center justify-center">
-                                            <div class="relative pl-[20px] w-[200px] md:w-[300px] lg:w-[350px] h-[200px] md:h-[350px] top-[0px] rounded-tr-[80px] bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_10%,#CBF9B6_100%)] overflow-hidden">
-                                                <div class="absolute bottom-[0px] right-[0px] h-100% md:w-[300px] lg:h-[350px]">
+                                        <div class="relative flex flex-row md:flex-col items-end md:items-center justify-center w-full">
+    
+                                            <!-- Left / Top Panel: Speaker Image Wrapper -->
+                                            <div class="relative pl-5 w-[200px] md:w-[300px] lg:w-[350px] h-[200px] md:h-[350px] rounded-tr-[80px] bg-gradient-to-b from-white via-white to-[#CBF9B6] overflow-hidden">
+                                                <div class="absolute bottom-0 right-0 w-full h-full">
                                                     <img class="w-full h-full object-cover" src="<?php echo esc_url(get_sub_field('speaker_image')); ?>" alt="<?php the_title(); ?>">
                                                 </div>
                                             </div>
-                                            <div class="w-[200px] md:w-[320px] lg:w-[400px] flex flex-col items-center rounded-br-[50px] justify-center gap-[10px] py-[20px] px-[20px] bg-[#1F773A] text-white">
-                                                <p class="text-center text-[18px] md:text-[22px] font-bold"><?php echo esc_html(get_sub_field('speaker_name')); ?></p>
-                                                <div class="h-[50px] w-[100%] md:w-[340px]">
-                                                    <p class="text-center font-[300] text-[12px] md:text-[16px]"><?php echo esc_html(get_sub_field('speaker_title')); ?></p>
+
+                                            <!-- Right / Bottom Panel: Speaker Info -->
+                                            <div class="flex-1 w-full md:w-[320px] lg:w-[400px] flex flex-col items-center rounded-br-[50px] justify-center gap-2.5 py-5 px-5 bg-[#1F773A] text-white">
+                                                <p class="text-center text-[18px] md:text-[22px] font-bold">
+                                                    <?php echo esc_html(get_sub_field('speaker_name')); ?>
+                                                </p>
+                                                <div class="w-full">
+                                                    <p class="text-center font-light text-[12px] md:text-[16px]">
+                                                        <?php echo esc_html(get_sub_field('speaker_title')); ?>
+                                                    </p>
                                                 </div>
                                             </div>
+
                                         </div>
+
                                     <?php endwhile; ?>
                                 <?php endif; ?> 
                             </div>
