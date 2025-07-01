b7d52213cc7365b34e429c019fc3e518906d0112
KenMirwell <kenmirwell@gmail.com>
2025-07-01 11:31:53 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -25,48 +25,16 @@ while (have_posts()) {
         </div>
         <div class="flex flex-col items-center gap-[20px] md:hidden">
             <img class="w-[70%]" src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="">
-            <p class="font-bold text-[#000000]">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
-            <div class="flex flex-col">
+            <p class="font-bold text-[#000000] text-center">Innovating for a Sustainable Future: Harnessing Technology and Alternative Solutions in Animal Nutrition and Health</p>
+            <div class="flex flex-col items-center justify-center">
                 <p class="font-bold text-[#1F773A] text-[28px]">SEPTEMBER 30, 2025</p>
                 <p class="font-bold text-[#EDB221] text-[18px]">Okada Manila, Paranaque City, Philippines</p>
             </div>
+            <?php get_template_part('2025-anual-event-template/components/count-down'); ?>  
             <a class="font-bold w-max bg-gradient-to-r from-[#1F773A] to-[#EDB221] text-[#ffffff] text-[18px] py-[10px] px-[50px] rounded-tl-[40px] rounded-br-[40px]" href="">Register</a>
         </div>
-        <div class="relative w-[100%] md:w-[50%] pb-[50px]">
-            <img class="hidden md:block z-[2]" src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
-            <img class="block md:hidden absolute z-[1] bottom-[-50px] transform scale-[1.3] opacity-[0.05]" src="https://philsan.org/wp-content/uploads/2025/06/Philsan-Ticket-BG@3x-8-2.png" alt="">
-            
-            <img class="block md:hidden" src="https://philsan.org/wp-content/uploads/2025/06/Asset-3@3x-8-1-scaled.png" alt="">
-            <div class="flex flex-col justify-end items-end pt-[50px]">
-                <div class="flex flex-col items-center gap-[10px]">
-                    <p class="text-[#1F773A] text-center font-bold">Event Starts at:</p>
-                    <div id="countdown" class="flex gap-4 text-[#1F773A]">
-                        <div class="flex flex-col items-center justify-center">
-                            <span class="font-[800] text-[42px]" id="days">00</span>
-                            <div class="text-sm">Days</div>
-                        </div>
-                        <div class="pt-[10px] text-[22px] font-bold">:</div>
-                        <div class="flex flex-col items-center justify-center">
-                            <span class="font-[800] text-[42px]" id="hours">00</span>
-                            <div class="text-sm">Hours</div>
-                        </div>
-                        <div class="pt-[10px] text-[22px] font-bold">:</div>
-                        <div class="flex flex-col items-center justify-center">
-                            <span class="font-[800] text-[42px]" id="minutes">00</span>
-                            <div class="text-sm">Minutes</div>
-                        </div>
-                        <div class="pt-[10px] text-[22px] font-bold">:</div>
-                        <div class="flex flex-col items-center justify-center">
-                            <span class="font-[800] text-[42px]" id="seconds">00</span>
-                            <div class="text-sm">Seconds</div>
-                        </div>
-                        <!-- <div class="flex flex-col itmes-center justify-center">
-                            <span id="milliseconds">000</span>
-                            <div class="text-sm">Milliseconds</div>
-                        </div> -->
-                    </div>
-                </div>
-            </div>
+        <div class="hidden md:block">
+            <?php get_template_part('2025-anual-event-template/components/count-down'); ?>  
         </div>
     </div>
 </div>
