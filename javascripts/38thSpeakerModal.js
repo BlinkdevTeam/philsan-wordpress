42db2e671530936f2a119a6376446af209099f58
KenMirwell <kenmirwell@gmail.com>
2025-07-02 14:46:28 +0800

    update

--- javascripts/38thSpeakerModal.js
+++ javascripts/38thSpeakerModal.js
@@ -2,34 +2,30 @@ class SpeakerModal {
     constructor() {
         this.modal = document.getElementById('dynamicModal');
     }
-    
+
     openModal({ title, image, content }) {
-        console.log("modal", modal);
-        modal.querySelector('#modalTitle').textContent = title;
-        modal.querySelector('#modalImage').src = image;
-        modal.querySelector('#modalContent').textContent = content;
-        modal.classList.remove('hidden');
+        console.log("modal", this.modal);
+        this.modal.querySelector('#modalTitle').textContent = title;
+        this.modal.querySelector('#modalImage').src = image;
+        this.modal.querySelector('#modalContent').textContent = content;
+        this.modal.classList.remove('hidden');
     }
 
-    closeModal(){
-        modal.classList.add('hidden');
+    closeModal() {
+        this.modal.classList.add('hidden');
     }
 
     handleModal() {
         document.getElementById("closeModal").addEventListener('click', () => {
-            console.log("modal trigerring")
             this.closeModal();
         });
 
         document.querySelectorAll('.speaker-item').forEach((card) => {
-            console.log("card", card)
-
             card.addEventListener('click', () => {
-                console.log("modal trigerring")
                 this.openModal({
                     title: card.dataset.name,
                     image: card.dataset.image,
-                    content: card.dataset.title, // or whatever you want to show
+                    content: card.dataset.title,
                 });
             });
         });
