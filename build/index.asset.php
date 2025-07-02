25131c41ac13a9d548b1ccf422c9b2c5ffad465c
KenMirwell <kenmirwell@gmail.com>
2025-07-02 12:21:25 +0800

    update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'b2404dcda542b2f6847c');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '374ecd2887a0704674b6');
