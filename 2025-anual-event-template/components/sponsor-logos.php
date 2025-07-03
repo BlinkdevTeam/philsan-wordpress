6f4efe88affbbaf9c5b7254433262deb42ffe3db
KenMirwell <kenmirwell@gmail.com>
2025-07-03 10:37:46 +0800

    update

--- 2025-anual-event-template/components/sponsor-logos.php
+++ 2025-anual-event-template/components/sponsor-logos.php
@@ -4,19 +4,19 @@
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
                 <p class="font-fraunces font-bold text-[#ffffff]">Diamond</p>
             </div>
-            <img class="w-[100%] md:w-[40%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226337.png" alt="">
+            <img class="w-[100%] md:w-[40%]" src="https://philsan.org/wp-content/uploads/2025/07/Diamond-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] gold-sponsor">
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
                 <p class="font-fraunces font-bold text-[#ffffff]">Gold</p>
             </div>
-            <img class="w-[100%] md:w-[40%] mt-[20px]" src="https://philsan.org/wp-content/uploads/2025/07/Frame-2147226397-scaled.png" alt="">
+            <img class="w-[100%] md:w-[40%] mt-[20px]" src="https://philsan.org/wp-content/uploads/2025/07/Gold-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] event-sponsor-1">
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
                 <p class="font-fraunces font-bold text-[#ffffff]">Event Sponsors</p>
             </div>
-            <img class="w-[100%] md:w-[60%]" src="https://philsan.org/wp-content/uploads/2025/07/Frame-2147226393-scaled.png" alt="">
+            <img class="w-[100%] md:w-[60%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226341-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] event-sponsor-2">
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
