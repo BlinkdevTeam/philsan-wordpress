f3e43766b8ee4513a06e1c33408cc43a27dccc07
KenMirwell <kenmirwell@gmail.com>
2025-07-03 13:15:51 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -351,7 +351,7 @@ while (have_posts()) {
 <div id="dynamicModal" class="fixed inset-0 z-50 hidden bg-white/50 flex items-center justify-center">
     <div class="relative w-[90%] lg:w-[70%]">
         <button id="closeModal" class="absolute flex justify-center items-center w-[50px] h-[50px] bg-[#ffffff] text-[38px]">&times;</button>
-        <div class="flex flex-col lg:flex-row items-center">
+        <div class="flex flex-col md:flex-row items-center">
             <div class="w-[100%] md:w-[50%] pt-[20px] bg-gradient-to-b from-white via-white to-[#CBF9B6] rounded-[50px]">
                 <div class="w-[90%] ml-[auto]">
                     <img id="modalImage" class="w-full h-auto" src="" alt="">
