87118b69820a9a3a5cd0ded5a55021c899497e47
KenMirwell <kenmirwell@gmail.com>
2025-07-03 09:52:12 +0800

    update

--- 2025-anual-event-template/components/sponsor-logos.php
+++ 2025-anual-event-template/components/sponsor-logos.php
@@ -16,7 +16,7 @@
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
                 <p class="font-fraunces font-bold text-[#ffffff]">Event Sponsors</p>
             </div>
-            <img class="w-[100%] md:w-[60%]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226341-scaled.png" alt="">
+            <img class="w-[100%] md:w-[60%]" src="https://philsan.org/wp-content/uploads/2025/07/Frame-2147226393-scaled.png" alt="">
         </div>
         <div class="swiper-slide p-[20px] event-sponsor-2">
             <div class="bg-[#1F773A] py-[5px] px-[10px] rounded-tl-xl rounded-br-xl">
