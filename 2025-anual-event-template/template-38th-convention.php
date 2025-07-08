72c2cd2403be9f56bd1feab0d9b47a1776cff0c9
KenMirwell <kenmirwell@gmail.com>
2025-07-08 12:32:18 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -122,24 +122,23 @@ while (have_posts()) {
                     </div>
                 <?php endif; ?>
 
-                <div class="swiper aboutSwiper">
-                    <div class="swiper-wrapper">
-                        <?php if ($gallery) : ?>
-                            <div class="about-gallery flex justify-between mt-[20px] gap-[20px]">
-                                <?php foreach ($gallery as $image) : ?>
-                                    <div class="swiper-slide">
-                                        <img 
-                                            src="<?php echo esc_url($image['url']); ?>" 
-                                            alt="<?php echo esc_attr($image['alt']); ?>" 
-                                            class="w-full h-auto object-cover rounded-xl"
-                                        />
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        <?php endif; ?>
+                <?php if ($gallery) : ?>
+                    <div class="swiper aboutSwiper">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($gallery as $image) : ?>
+                                <div class="swiper-slide">
+                                    <img 
+                                        src="<?php echo esc_url($image['url']); ?>" 
+                                        alt="<?php echo esc_attr($image['alt']); ?>" 
+                                        class="w-full h-auto object-cover rounded-xl"
+                                    />
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="swiper-pagination mt-4"></div>
                     </div>
-                    <div class="swiper-pagination"></div>
-                </div>
+                <?php endif; ?>
+
             </div>
         <?php endif; ?>
     </div>
