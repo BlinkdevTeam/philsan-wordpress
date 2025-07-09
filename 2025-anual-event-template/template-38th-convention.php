2953a6549ec6a11a097af50cd073588d32a116e4
KenMirwell <kenmirwell@gmail.com>
2025-07-09 12:06:10 +0800

    update

--- 2025-anual-event-template/template-38th-convention.php
+++ 2025-anual-event-template/template-38th-convention.php
@@ -9,11 +9,7 @@ while (have_posts()) {
     $page = get_page_by_title('38th Annual Convention');
 ?>
 
-<div class="relative hero-section flex items-center 
-    bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] 
-    hover:bg-[linear-gradient(to_top,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] 
-    transition-colors duration-700 ease-in-out overflow-hidden"
->
+<div class="relative hero-section flex items-center bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_60%,#CBF9B6_100%)] overflow-hidden">
     <div class="relative z-[1] flex flex-col md:flex-row gap-[50px] w-[90%] max-w-[1280px] mx-auto py-[120px] lg:py-[0px] pt-[60px]">
         <div class="hidden md:flex flex-col w-[50%] gap-[40px]">
             <img class="w-[80px] ml-[-15px]" src="https://philsan.org/wp-content/uploads/2025/06/Frame-2147226390.png" alt="">
@@ -25,7 +21,16 @@ while (have_posts()) {
                 <p class="font-bold text-[#1F773A] text-[28px]">SEPTEMBER 30, 2025</p>
                 <p class="font-bold text-[#EDB221] text-[18px]">Okada Manila, Paranaque City, Philippines</p>
             </div>
-            <a class="font-bold w-max bg-gradient-to-r from-[#1F773A] to-[#EDB221] text-[#ffffff] text-[24px] lg:text-[38px] py-[10px] px-[50px] rounded-tl-[40px] rounded-br-[40px]" href="https://philsan.org/38th-annual-convention/registration/">Register</a>
+            <a href="https://philsan.org/38th-annual-convention/registration/"
+                class="font-bold w-max 
+                        bg-gradient-to-r from-[#1F773A] to-[#EDB221] 
+                        hover:bg-gradient-to-l 
+                        transition-all duration-500 ease-in-out
+                        text-[#ffffff] text-[24px] lg:text-[38px] 
+                        py-[10px] px-[50px] 
+                        rounded-tl-[40px] rounded-br-[40px]">
+                Register
+            </a>
         </div>
         <div class="flex flex-col items-center gap-[20px] md:hidden">
             <img class="w-[70%]" src="https://philsan.org/wp-content/uploads/2025/06/Asset-2-1.png" alt="">
