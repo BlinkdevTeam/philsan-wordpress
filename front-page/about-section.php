26a3578b5709c2100a1d9bb2b774bcba6814e94a
KenMirwell <kenmirwell@gmail.com>
2025-08-12 14:18:35 +0800

    update about section

--- front-page/about-section.php
+++ front-page/about-section.php
@@ -1,20 +1,8 @@
+<?php 
+    $group = get_field('about_section');
+?>
 <div class="about-section h-[500px] lg:h-[720px]">
-    <?php if ( have_rows('hero_section') ): ?>
-        <?php while ( have_rows('hero_section') ): the_row(); ?>
-
-            <?php if ( get_row_layout() == 'hero_item_group' ): ?>
-                <div class="flex space-between h-full w-full relative y-thumbnail">
-                    <div class="container py-[100px] mx-auto z-[2]">
-                        <div class="w-[50%] flex flex-col justify-center h-[100%]">
-                            <h1 class="leading-[normal] text-[48px] xl:text-[72px] font-[700] pb-[20px] text-[#ffc200]"><?php the_sub_field('hero_title'); ?></h1>
-                            <p class="text-[18px] xl:text-[24px] text-[#ffffff]"><?php the_sub_field('hero_sub'); ?></p>
-                        </div>
-                    </div>
-                    <div class="bg-gradient-to-b from-[rgba(11,83,4,0.3)] to-[rgba(11,83,4,1)] w-full h-full absolute top-0 left-0 z-[1]"></div>
-                    <img class="absolute w-full h-full object-cover" src="<?php echo get_sub_field('hero_image'); ?>" alt="hero-image">
-                </div>
-            <?php endif; ?>
-
-        <?php endwhile; ?>
+    <?php if (!empty($group['title'])) : ?>
+        <h2><?php echo esc_html($group['title']) ?></h2>
     <?php endif; ?>
 </div>
--- front-page/figures-section.php
+++ front-page/figures-section.php
@@ -3,8 +3,8 @@
         <?php if ( have_rows('figures_section') ): ?>
             <?php while ( have_rows('figures_section') ): the_row(); ?>
                 <div class="flex flex-col items-center justify-center">
-                    <h2 class="text-[42px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
-                    <p class="24px font-[600]"><?php the_sub_field("figures_description") ?></p>
+                    <h2 class="text-[48px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
+                    <p class="text-[24px] font-[600]"><?php the_sub_field("figures_description") ?></p>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
