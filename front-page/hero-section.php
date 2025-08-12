a49d7250eb886ffebd4f6170e167a8a61da99d78
KenMirwell <kenmirwell@gmail.com>
2025-08-12 10:48:14 +0800

    update

--- front-page/hero-section.php
+++ front-page/hero-section.php
@@ -1,27 +1,14 @@
-<?php
-$home_page = get_page_by_title('Home');
-if ( $home_page ) :
-    $page_id = $home_page->ID;
-    $hero = get_field('hero_section', $page_id);
+<div class="home-hero-section">
+    <?php if ( have_rows('hero_blocks') ): ?>
+        <?php while ( have_rows('hero_blocks') ): the_row(); ?>
 
-    if ( $hero ) :
-        $img = $hero['hero_background'];
-        $header = $hero['hero_header'];
-        $details = $hero['header_details'];
-?>
+            <?php if ( get_row_layout() == 'hero_item_group' ): ?>
+                <div class="hero">
+                    <h1><?php the_sub_field('hero_title'); ?></h1>
+                    <p><?php the_sub_field('hero_sub'); ?></p>
+                </div>
+            <?php endif; ?>
 
-<div class="relative w-full rounded-lg overflow-hidden shadow-lg mb-12">
-    <?php if ( $img ) : ?>
-        <img src="<?php echo esc_url($img['url']); ?>" alt="" class="w-full h-auto object-cover">
+        <?php endwhile; ?>
     <?php endif; ?>
-    <div class="absolute inset-0 bg-black/50 flex flex-col justify-center items-center text-center text-white px-4">
-        <?php if ( $header ) : ?>
-            <h2 class="text-4xl font-bold mb-2"><?php echo esc_html($header); ?></h2>
-        <?php endif; ?>
-        <?php if ( $details ) : ?>
-            <p class="text-lg max-w-xl"><?php echo wp_kses_post($details); ?></p>
-        <?php endif; ?>
-    </div>
 </div>
-
-<?php endif; endif; ?>
