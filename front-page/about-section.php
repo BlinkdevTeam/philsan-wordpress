955997464ecdb98d8a116cc8f2a317ced4bfc2b6
KenMirwell <kenmirwell@gmail.com>
2025-08-12 14:38:36 +0800

    update

--- front-page/about-section.php
+++ front-page/about-section.php
@@ -8,7 +8,7 @@
                 <h2 class="text-[24px] font-[600]"><?php echo esc_html($group['title']) ?></h2>
             <?php endif; ?>
             <?php if (!empty($group['description'])) : ?>
-                <p class="text-[48px] text-[#1F773A] font-[700] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+                <p class="text-[34px] text-[#1F773A] font-[700] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
             <?php endif; ?>
             <?php if (!empty($group['button_title'])) : ?>
                 <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold bg-[#FFC200] p-[20px] rounded-tl-[80px] rounded-br-[80px]"><?php echo esc_html($group['button_title']) ?></a>
