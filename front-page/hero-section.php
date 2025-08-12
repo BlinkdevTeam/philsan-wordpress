cb1ffba1c4f775f05af63409f35456456f9185fd
KenMirwell <kenmirwell@gmail.com>
2025-08-12 11:36:12 +0800

    update

--- front-page/hero-section.php
+++ front-page/hero-section.php
@@ -3,14 +3,16 @@
         <?php while ( have_rows('hero_section') ): the_row(); ?>
 
             <?php if ( get_row_layout() == 'hero_item_group' ): ?>
-                <div class="w-[80%] mb-[100px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] mx-auto z-[2]">
-                    <div class="">
-                        <h1><?php the_sub_field('hero_title'); ?></h1>
-                        <p><?php the_sub_field('hero_sub'); ?></p>
+                <div class="flex space-between h-full w-full relative y-thumbnail">
+                    <div class="w-[80%] mb-[100px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] mx-auto z-[2]">
+                        <div class="">
+                            <h1><?php the_sub_field('hero_title'); ?></h1>
+                            <p><?php the_sub_field('hero_sub'); ?></p>
+                        </div>
                     </div>
+                    <div class="bg-gradient-to-l from-[rgba(0,0,0,0.2)] to-[rgba(0,0,0,0.7)] w-full h-full absolute top-0 left-0 z-[1]"></div>
+                    <img class="absolute w-full h-full object-cover" src="<?php echo get_sub_field('hero_image'); ?>" alt="hero-image">
                 </div>
-                <div class="bg-gradient-to-l from-[rgba(0,0,0,0.2)] to-[rgba(0,0,0,0.7)] w-full h-full absolute top-0 left-0 z-[1]"></div>
-                <img class="absolute w-full h-full object-cover" src="<?php echo get_sub_field('hero_image'); ?>" alt="hero-image">
             <?php endif; ?>
 
         <?php endwhile; ?>
