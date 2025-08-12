cbdcb0c1f39eab932432809245485a0385c20935
KenMirwell <kenmirwell@gmail.com>
2025-08-12 16:28:55 +0800

    update

--- front-page/about-section.php
+++ front-page/about-section.php
@@ -1,20 +1,22 @@
 <?php 
     $group = get_field('about_section');
 ?>
-<div class="about-section py-[20px]">
+<div class="about-section pt-[50px]">
     <div class="flex container mx-auto gap-[50px]">
-        <div class="w-[45%]">
+        <div class="w-[40%]">
             <?php if (!empty($group['title'])) : ?>
                 <h2 class="text-[24px] font-[600]"><?php echo esc_html($group['title']) ?></h2>
             <?php endif; ?>
             <?php if (!empty($group['description'])) : ?>
                 <p class="text-[34px] text-[#1F773A] font-[700] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
             <?php endif; ?>
-            <?php if (!empty($group['button_title'])) : ?>
-                <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold bg-[#FFC200] p-[20px] rounded-tl-[80px] rounded-br-[80px]"><?php echo esc_html($group['button_title']) ?></a>
-            <?php endif; ?>
+            <div class="pt-[20px]">
+                <?php if (!empty($group['button_title'])) : ?>
+                    <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold bg-[#FFC200] p-[20px] rounded-tl-[80px] rounded-br-[80px]"><?php echo esc_html($group['button_title']) ?></a>
+                <?php endif; ?>
+            </div>
         </div>
-        <div class="w-[55%]">
+        <div class="w-[60%]">
             <div class="flex gap-[20px]">
                 <?php if ( !empty($group['items']) && is_array($group['items']) ) : ?>
                     <?php foreach ( $group['items'] as $item ) : ?>
