2adc1d696f80818d49d24a5c11fbbd0bdcc01b72
KenMirwell <kenmirwell@gmail.com>
2025-08-12 12:01:53 +0800

    update

--- front-page/hero-section.php
+++ front-page/hero-section.php
@@ -5,9 +5,9 @@
             <?php if ( get_row_layout() == 'hero_item_group' ): ?>
                 <div class="flex space-between h-full w-full relative y-thumbnail">
                     <div class="w-[80%] py-[100px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] mx-auto z-[2]">
-                        <div class="">
-                            <h1 class="font-[600] text-[#ffc200]"><?php the_sub_field('hero_title'); ?></h1>
-                            <p class="font-[600] text-[#ffffff]"><?php the_sub_field('hero_sub'); ?></p>
+                        <div class="w-[50%]">
+                            <h1 class="text-[42px] font-[600] text-[#ffc200]"><?php the_sub_field('hero_title'); ?></h1>
+                            <p class="text-[18px] font-[600] text-[#ffffff]"><?php the_sub_field('hero_sub'); ?></p>
                         </div>
                     </div>
                     <div class="bg-gradient-to-b from-[rgba(255,255,255,0.0)] to-[rgba(11,83,4,0.8)] w-full h-full absolute top-0 left-0 z-[1]"></div>
