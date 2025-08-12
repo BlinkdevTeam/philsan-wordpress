78c441d7b44bc2a6225cd951d9360445d5941291
KenMirwell <kenmirwell@gmail.com>
2025-08-12 13:37:23 +0800

    update

--- front-page/figures-section.php
+++ front-page/figures-section.php
@@ -1,9 +1,15 @@
-<div class="home-figures-section">
+<div class="home-figures-section container">
     <div class="flex justify-between">
         <?php if ( have_rows('figures_section') ): ?>
             <?php while ( have_rows('figures_section') ): the_row(); ?>
-                <div class="">
-                    <h2><?php the_sub_field("figures") ?></h2>
+                <div class="flex flex-col items-center justify-center">
+                    <div class="flex gap-[10px]">
+                        <h2 class="18px font-[600]"><?php the_sub_field("figures") ?></h2>
+                        <h2 class="18px font-[600]"><?php the_sub_field("figures_Unit") ?></h2>
+                    </div>
+                    <div>
+                        <p><?php the_sub_field("figures_description") ?></p>
+                    </div>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
