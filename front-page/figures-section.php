20b523c49c033a351f301d85ec3a5273c91df350
KenMirwell <kenmirwell@gmail.com>
2025-08-12 13:44:44 +0800

    update

--- front-page/figures-section.php
+++ front-page/figures-section.php
@@ -3,13 +3,8 @@
         <?php if ( have_rows('figures_section') ): ?>
             <?php while ( have_rows('figures_section') ): the_row(); ?>
                 <div class="flex flex-col items-center justify-center">
-                    <div class="flex gap-[10px]">
-                        <h2 class="text-[24px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?></h2>
-                        <h2 class="text-[18px] font-[600] text-center"><?php the_sub_field("figures_unit") ?></h2>
-                    </div>
-                    <div>
-                        <p><?php the_sub_field("figures_description") ?></p>
-                    </div>
+                    <h2 class="text-[42px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
+                    <p class="24px"><?php the_sub_field("figures_description") ?></p>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
