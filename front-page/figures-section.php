d6afa8f572da3d47cab3176e5487f2e2c00d9c42
KenMirwell <kenmirwell@gmail.com>
2025-08-12 14:04:35 +0800

    update

--- front-page/figures-section.php
+++ front-page/figures-section.php
@@ -1,10 +1,10 @@
 <div class="home-figures-section">
-    <div class="flex justify-between container mx-auto py-[50px]">
+    <div class="flex justify-between w-[720px] lg:w-[1080px] mx-auto py-[50px]">
         <?php if ( have_rows('figures_section') ): ?>
             <?php while ( have_rows('figures_section') ): the_row(); ?>
                 <div class="flex flex-col items-center justify-center">
                     <h2 class="text-[42px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
-                    <p class="24px"><?php the_sub_field("figures_description") ?></p>
+                    <p class="24px font-[600]"><?php the_sub_field("figures_description") ?></p>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
