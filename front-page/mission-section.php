cb9a5241b4498c650f5490448feedf09aa9473cf
KenMirwell <kenmirwell@gmail.com>
2025-08-12 16:52:20 +0800

    update

--- front-page/mission-section.php
+++ front-page/mission-section.php
@@ -1,37 +1,29 @@
-<?php
-$home_page = get_page_by_title('Home');
-if ( $home_page ) :
-    $page_id = $home_page->ID;
-    $mission = get_field('mission_statement_section', $page_id);
-
-    if ( $mission ) :
-        $img1 = $mission['back_image'];
-        $img2 = $mission['front_image'];
-        $header = $mission['header'];
-        $details = $mission['header_details'];
+<?php 
+    $group = get_field('mission_section');
 ?>
-
-<div class="w-full rounded-lg overflow-hidden p-6 shadow-lg mb-12">
-    <div class="grid grid-cols-2">
-        <div class="relative w-full py-6">
-            <?php if ( $img1 ) : ?>
-                <img src="<?php echo esc_url($img1['url']); ?>" alt="" class="w-[409px] h-auto object-cover">
+<div class="mission-section pt-[100px]">
+    <div class="flex container mx-auto gap-[50px]">
+        <div class="w-[40%]">
+            <?php if (!empty($group['title'])) : ?>
+                <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
             <?php endif; ?>
-            
-            <?php if ( $img2 ) : ?>
-                <img src="<?php echo esc_url($img2['url']); ?>" alt=""
-                    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[200px] h-auto z-10 object-cover">
-            <?php endif; ?>
+            <div class="pt-[10px]">
+                <?php if (!empty($group['description'])) : ?>
+                    <p class="text-[34px] font-[600] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="pt-[40px]">
+                <?php if (!empty($group['button_title'])) : ?>
+                    <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
+                <?php endif; ?>
+            </div>
         </div>
-        <div class="flex flex-col justify-center items-start text-start px-4">
-            <?php if ( $header ) : ?>
-                <h2 class="text-4xl font-bold mb-2"><?php echo esc_html($header); ?></h2>
-            <?php endif; ?>
-            <?php if ( $details ) : ?>
-                <p class="text-lg max-w-xl"><?php echo wp_kses_post($details); ?></p>
-            <?php endif; ?>
+        <div class="w-[60%]">
+            <div class="w-[25%] image-container">
+                <div class="h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
+                    <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>">
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
-<?php endif; endif; ?>
