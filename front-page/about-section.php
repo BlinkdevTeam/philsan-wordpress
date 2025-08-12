ba7a2ca5fb095ef3e008cce4a6eb66d12d0593a5
KenMirwell <kenmirwell@gmail.com>
2025-08-12 15:43:36 +0800

    update

--- front-page/about-section.php
+++ front-page/about-section.php
@@ -1,9 +1,9 @@
 <?php 
     $group = get_field('about_section');
 ?>
-<div class="about-section h-[500px] lg:h-[720px] py-[20px]">
+<div class="about-section py-[20px]">
     <div class="flex container mx-auto">
-        <div class="">
+        <div class="w-[45%]">
             <?php if (!empty($group['title'])) : ?>
                 <h2 class="text-[24px] font-[600]"><?php echo esc_html($group['title']) ?></h2>
             <?php endif; ?>
@@ -14,8 +14,20 @@
                 <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold bg-[#FFC200] p-[20px] rounded-tl-[80px] rounded-br-[80px]"><?php echo esc_html($group['button_title']) ?></a>
             <?php endif; ?>
         </div>
-        <div>
-            
+        <div class="w-[55%] flex flex-wrap">
+            <?php if ( have_rows('items', 'about_section') ) : ?>
+                <?php while ( have_rows('items', 'about_section') ) : the_row(); ?>
+                    <?php 
+                        $image = get_sub_field('image');
+                    ?>
+                        <div class="w-[100%] lg:w-[50%] image-container">
+                            <div class="h-[200px] md:h-[320px] lg:h-[450px] rounded-lg overflow-hidden">
+                                <img class="w-full h-full object-cover" src="<?php echo esc_url($image); ?>" alt="About items image">
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
