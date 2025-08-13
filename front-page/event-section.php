31f3a317e0533eac1784ae39afeb8636b3f18622
KenMirwell <kenmirwell@gmail.com>
2025-08-13 13:51:17 +0800

    update

--- front-page/event-section.php
+++ front-page/event-section.php
@@ -11,13 +11,15 @@
     ));
 ?>
 <div class="event-section pt-[100px] pb-[50px]">
-    <?php if ($events->have_posts()) : ?>
-        <?php while ($events->have_posts()): ?>
-            <?php $events->the_post(); ?>
-            <?php $event_id = get_the_ID(); ?>
-            <?php var_dump($event_id); ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <div class="flex container mx-auto gap-[50px]">
+        <?php if ($events->have_posts()) : ?>
+            <?php while ($events->have_posts()): ?>
+                <?php $events->the_post(); ?>
+                <?php $event_id = get_the_ID(); ?>
+                <?php var_dump($event_id); ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 </div>
 
     
