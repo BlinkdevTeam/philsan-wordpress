0a5005047a0240308be197ffc14b1569baddca68
KenMirwell <kenmirwell@gmail.com>
2025-08-13 13:40:39 +0800

    update

--- front-page/event-section.php
+++ front-page/event-section.php
@@ -1,42 +1,20 @@
 <?php
-$home_page = get_page_by_title('Home');
-if ( $home_page ) :
-    $page_id = $home_page->ID;
-    $event = get_field('event_section', $page_id);
-
-    if ( $event ) :
-        $header = $event['header'];
-        $details = $event['header_details'];
+    $events = new WP_Query(array(
+        "post_type" => "events",
+        // "tax_query" => array(
+        //     array(
+        //         "taxonomy" => "km_category",
+        //         "field"    => "slug",
+        //         "terms"    => "Featured",
+        //     ),
+        // ),
+    ));
 ?>
-
-<div class="w-full flex flex-col justify-center items-center rounded-lg overflow-hidden p-6 shadow-lg mb-12">
-    <div class="flex">
-        <div class="flex flex-col justify-center items-center text-center px-4">
-            <?php if ( $header ) : ?>
-                <h2 class="text-4xl font-bold mb-2"><?php echo esc_html($header); ?></h2>
-            <?php endif; ?>
-            <?php if ( $details ) : ?>
-                <p class="text-lg max-w-xl"><?php echo wp_kses_post($details); ?></p>
-            <?php endif; ?>
-        </div>
-    </div>
-
-
-    <?php 
-    $image_highlights = $event['image_highlights'];
-    if ( $image_highlights ) : ?>
-        <div class="flex flex-wrap gap-6 justify-center py-4">
-            <?php foreach ( $image_highlights as $row ) :
-                $img = $row['image'];
-                if ( $img ) :
-            ?>
-                <img src="<?php echo esc_url($img['url']); ?>" alt=""
-                    class="w-[209px] h-auto object-cover rounded shadow">
-            <?php endif; endforeach; ?>
-        </div>
-    <?php endif; ?>
-
-
-</div>
-
-<?php endif; endif; ?>
+<?php if ($events->have_posts()) : ?>
+    <?php while ($events->have_posts()): ?>
+        <?php $events->the_post(); ?>
+        <?php $event_id = get_the_ID(); ?>
+        <?php var_dump($event_id); ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+    
--- front-page/figures-section.php
+++ front-page/figures-section.php
@@ -4,7 +4,7 @@
             <?php while ( have_rows('figures_section') ): the_row(); ?>
                 <div class="flex flex-col items-center justify-center">
                     <h2 class="text-[48px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
-                    <p class="text-[24px] font-[600]"><?php the_sub_field("figures_description") ?></p>
+                    <p class="text-[24px] text-center font-[600]"><?php the_sub_field("figures_description") ?></p>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
