8a338fe89ebd51ecdea1a2db323ab79584a0547d
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-08-14 15:26:44 +0800

    update

--- front-page/news-section.php
+++ front-page/news-section.php
@@ -1,7 +1,7 @@
 <?php
     $news = new WP_Query(array(
         "post_type" => "news",
-        "posts_per_page" => 5,
+        "posts_per_page" => 2,
         'order' => 'ASC',     
     ));
 ?>
@@ -25,20 +25,22 @@
                           $description = get_field("description");
                       ?>
 
-                      <div class="flex flex-col gap-[20px]">
-                          <div>
-                            <h2 class="text-[24px] font-[300]"><?php the_title(); ?></h2>
-                            <div class="pt-[40px]">
-                              <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl">Learn More</a>
+                      <div class="p-[40px] rounded-xl bg-[#ffffff]">
+                        <div class="flex flex-col gap-[20px]">
+                            <div class="flex justify-between">
+                              <h2 class="text-[24px] font-[300]"><?php the_title(); ?></h2>
+                              <div class="pt-[40px]">
+                                <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl">Learn More</a>
+                              </div>
                             </div>
-                          </div>
-                          <div class="relative w-full h-[200px] md:h-[320px] lg:h-[450px] overflow-hidden rounded-tl-2xl rounded-br-2xl">
-                              <div class="bg-[#000000] opacity-[0.4] w-full h-full absolute top-0 left-0 z-[1]"></div>
-                              <img class="w-full h-full object-cover" src="<?php echo esc_url($image); ?>" alt="event image">
-                          </div>
-                          <div class="flex flex-col gap-[10px]">
-                              <p class="text-[34px] font-[600] leading-normal"><?php echo esc_html($description); ?></p>
-                          </div>
+                            <div class="relative w-full h-[200px] md:h-[320px] lg:h-[450px] overflow-hidden rounded-tl-2xl rounded-br-2xl">
+                                <div class="bg-[#000000] opacity-[0.4] w-full h-full absolute top-0 left-0 z-[1]"></div>
+                                <img class="w-full h-full object-cover" src="<?php echo esc_url($image); ?>" alt="event image">
+                            </div>
+                            <div class="flex flex-col gap-[10px]">
+                                <p class="text-[34px] font-[600] leading-normal"><?php echo esc_html($description); ?></p>
+                            </div>
+                        </div>
                       </div>
                   <?php endwhile; ?>
                   <?php wp_reset_postdata(); ?>
