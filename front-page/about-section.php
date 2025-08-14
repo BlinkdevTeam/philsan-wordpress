222a900d0522b10bf3704c542cc09ce44e6d0aac
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-08-14 15:30:04 +0800

    update

--- front-page/about-section.php
+++ front-page/about-section.php
@@ -1,41 +1,43 @@
 <?php 
     $group = get_field('about_section');
 ?>
-<div class="about-section pt-[100px] pb-[50px]">
-    <div class="flex custom-container mx-auto gap-[50px]">
-        <div class="w-[40%]">
-            <?php if (!empty($group['title'])) : ?>
-                <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
-            <?php endif; ?>
-            <div class="pt-[10px]">
-                <?php if (!empty($group['description'])) : ?>
-                    <p class="text-[34px] text-[500] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+<div class="bg-[#ffffff]">
+    <div class="about-section pt-[100px] pb-[50px]">
+        <div class="flex custom-container mx-auto gap-[50px]">
+            <div class="w-[40%]">
+                <?php if (!empty($group['title'])) : ?>
+                    <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
                 <?php endif; ?>
+                <div class="pt-[10px]">
+                    <?php if (!empty($group['description'])) : ?>
+                        <p class="text-[34px] text-[500] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+                    <?php endif; ?>
+                </div>
+                <div class="pt-[40px]">
+                    <?php if (!empty($group['button_title'])) : ?>
+                        <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div class="pt-[40px]">
-                <?php if (!empty($group['button_title'])) : ?>
-                    <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
-                <?php endif; ?>
-            </div>
-        </div>
-        <div class="w-[60%] overflow-hidden">
-            <div class="flex gap-[20px]">
-                <?php if ( !empty($group['items']) && is_array($group['items']) ) : ?>
-                    <?php foreach ( $group['items'] as $item ) : ?>
-                        <?php 
-                            $image = $item['item_image']; // Replace 'image' with your sub field name
-                            if ( !empty($image) ) :
-                                $image_url = is_array($image) ? $image['url'] : $image;
-                                $alt = is_array($image) && !empty($image['alt']) ? $image['alt'] : get_the_title();
-                        ?>
-                            <div class="image-container">
-                                <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
-                                    <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>">
+            <div class="w-[60%] overflow-hidden">
+                <div class="flex gap-[20px]">
+                    <?php if ( !empty($group['items']) && is_array($group['items']) ) : ?>
+                        <?php foreach ( $group['items'] as $item ) : ?>
+                            <?php 
+                                $image = $item['item_image']; // Replace 'image' with your sub field name
+                                if ( !empty($image) ) :
+                                    $image_url = is_array($image) ? $image['url'] : $image;
+                                    $alt = is_array($image) && !empty($image['alt']) ? $image['alt'] : get_the_title();
+                            ?>
+                                <div class="image-container">
+                                    <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
+                                        <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>">
+                                    </div>
                                 </div>
-                            </div>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
--- front-page/mission-section.php
+++ front-page/mission-section.php
@@ -1,27 +1,29 @@
 <?php 
     $group = get_field('mission_section');
 ?>
-<div class="mission-section pt-[100px] pb-[50px]">
-    <div class="flex custom-container mx-auto gap-[50px]">
-        <div class="w-[40%]">
-            <?php if (!empty($group['title'])) : ?>
-                <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
-            <?php endif; ?>
-            <div class="pt-[10px]">
-                <?php if (!empty($group['description'])) : ?>
-                    <p class="text-[34px] text-[500] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+<div class="bg-[#ffffff]">
+    <div class="mission-section pt-[100px] pb-[50px]">
+        <div class="flex custom-container mx-auto gap-[50px]">
+            <div class="w-[40%]">
+                <?php if (!empty($group['title'])) : ?>
+                    <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
                 <?php endif; ?>
+                <div class="pt-[10px]">
+                    <?php if (!empty($group['description'])) : ?>
+                        <p class="text-[34px] text-[500] leading-[normal]"><?php echo esc_html($group['description']) ?></p>
+                    <?php endif; ?>
+                </div>
+                <div class="pt-[40px]">
+                    <?php if (!empty($group['button_title'])) : ?>
+                        <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div class="pt-[40px]">
-                <?php if (!empty($group['button_title'])) : ?>
-                    <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
-                <?php endif; ?>
-            </div>
-        </div>
-        <div class="w-[60%]">
-            <div class="w-[100%] image-container">
-                <div class="h-[200px] md:h-[320px] lg:h-[450px] rounded-2xl overflow-hidden">
-                    <img class="w-full h-full object-cover" src="<?php echo esc_html($group['image']) ?>" alt="<?php echo esc_attr($alt); ?>">
+            <div class="w-[60%]">
+                <div class="w-[100%] image-container">
+                    <div class="h-[200px] md:h-[320px] lg:h-[450px] rounded-2xl overflow-hidden">
+                        <img class="w-full h-full object-cover" src="<?php echo esc_html($group['image']) ?>" alt="<?php echo esc_attr($alt); ?>">
+                    </div>
                 </div>
             </div>
         </div>
