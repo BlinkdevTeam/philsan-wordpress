cc00570c1e957d6d75f3e71b3e4ec66f13f04014
KenMirwell <kenmirwell@gmail.com>
2025-08-18 13:29:18 +0800

    update

--- about-page/organization-section.php
+++ about-page/organization-section.php
@@ -12,20 +12,22 @@
             </div>
 
             <?php if ( have_rows('member') ): ?> 
-                <?php while ( have_rows('member') ): the_row(); ?>
-                    <?php 
-                        $image_url = get_sub_field('image');
-                        $name = get_sub_field('name');
-                        $job_title = get_sub_field('job_title');
-                    ?>
-                    <div class="image-container">
-                        <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
-                            <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="member image">
+                <div class="flex flex-wrap gap-[20px]">
+                    <?php while ( have_rows('member') ): the_row(); ?>
+                        <?php 
+                            $image_url = get_sub_field('image');
+                            $name = get_sub_field('name');
+                            $job_title = get_sub_field('job_title');
+                        ?>
+                        <div class="image-container w-[25%]">
+                            <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
+                                <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="member image">
+                            </div>
+                            <p class="text-[20px] font-[600] mt-2"><?php echo esc_html($name); ?></p>
+                            <p class="text-[16px] text-gray-600"><?php echo esc_html($job_title); ?></p>
                         </div>
-                        <p class="text-[20px] font-[600] mt-2"><?php echo esc_html($name); ?></p>
-                        <p class="text-[16px] text-gray-600"><?php echo esc_html($job_title); ?></p>
-                    </div>
-                <?php endwhile; ?> 
+                    <?php endwhile; ?> 
+                </div>
             <?php endif; ?>
 
         <?php endwhile; ?>
