b460ce3e7332be9e6d27a0e765b2bb210cea087a
KenMirwell <kenmirwell@gmail.com>
2025-08-18 13:20:10 +0800

    update

--- about-page/organization-section.php
+++ about-page/organization-section.php
@@ -1,25 +1,33 @@
 <div class="organization-section custom-container py-[50px]">
     <?php if ( have_rows('organization_repeater') ): ?>
         <?php while ( have_rows('organization_repeater') ): the_row(); ?>
+            
             <div class="flex flex-col items-center justify-center">
-                <h2 class="text-[48px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("title") ?><?php the_sub_field("figures_unit") ?></h2>
-                <p class="text-[24px] text-center font-[600]"><?php the_sub_field("sub") ?></p>
+                <h2 class="text-[48px] text-[#1F773A] font-[700] text-center">
+                    <?php the_sub_field("title") ?>
+                </h2>
+                <p class="text-[24px] text-center font-[600]">
+                    <?php the_sub_field("sub") ?>
+                </p>
             </div>
 
-            <?php if ( have_rows('member') ): ?>
+            <?php if ( have_rows('member') ): ?> 
                 <?php while ( have_rows('member') ): the_row(); ?>
-                <?php 
-                    $image_url = get_sub_field('image');
-                    $name = get_sub_field('name');
-                    $job_title = get_sub_field('job_title');
-                ?>
+                    <?php 
+                        $image_url = get_sub_field('image');
+                        $name = get_sub_field('name');
+                        $job_title = get_sub_field('job_title');
+                    ?>
                     <div class="image-container">
                         <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
                             <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="member image">
                         </div>
+                        <p class="text-[20px] font-[600] mt-2"><?php echo esc_html($name); ?></p>
+                        <p class="text-[16px] text-gray-600"><?php echo esc_html($job_title); ?></p>
                     </div>
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    <?php endwhile; ?>
+                <?php endwhile; ?> 
+            <?php endif; ?>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
 </div>
