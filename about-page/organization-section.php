60175c0b90f3a1849fbcea632b661834b9777687
KenMirwell <kenmirwell@gmail.com>
2025-08-18 12:05:17 +0800

    update

--- about-page/organization-section.php
+++ about-page/organization-section.php
@@ -1,50 +1,25 @@
-<?php 
-    $group = get_field('organization_group');
-?>
 <div class="text-icon-section custom-container py-[50px]">
-    <div class="flex space-between">
-        <div class="">
-            <?php if (!empty($group['title'])) : ?>
-                <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
-            <?php endif; ?>
-            <div class="pt-[18px]">
-                <?php if (!empty($group['sub'])) : ?>
-                    <p class="text-[34px] text-[500] leading-[34px]"><?php echo esc_html($group['sub']) ?></p>
-                <?php endif; ?>
+    <?php if ( have_rows('organizaiton_repeater') ): ?>
+        <?php while ( have_rows('organizaiton_repeater') ): the_row(); ?>
+            <div class="flex flex-col items-center justify-center">
+                <h2 class="text-[48px] text-[#1F773A] font-[700] text-center"><?php the_sub_field("figures") ?><?php the_sub_field("figures_unit") ?></h2>
+                <p class="text-[24px] text-center font-[600]"><?php the_sub_field("figures_description") ?></p>
             </div>
-        </div>
-    </div>
-    <!-- Repeater section -->
-    <?php if( !empty($group['member']) && is_array($group['member']) ): ?>
-        <div class="flex gap-[20px] justify-center">
-            <?php foreach( $group['member'] as $item ): ?>
-                <div class="flex gap-[20px] items-start w-[25%]">
-                    <?php if( !empty($item['icon']) ): ?>
-                        <div class="bg-[#CBF9B6] p-[20px] rounded-full overflow-hidden">
-                            <img src="<?php echo esc_url($item['icon']['url']); ?>" alt="icon image" class="w-10 h-10 object-contain" />
+
+            <?php if ( have_rows('member') ): ?>
+                <?php while ( have_rows('member') ): the_row(); ?>
+                <?php 
+                    $image_url = get_sub_field('image');
+                    $name = get_sub_field('name');
+                    $job_title = get_sub_field('job_title');
+                ?>
+                    <div class="image-container">
+                        <div class="w-[350px] h-[200px] md:h-[320px] lg:h-[450px] rounded-tl-2xl rounded-br-2xl overflow-hidden">
+                            <img class="w-full h-full object-cover" src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>">
                         </div>
-                    <?php endif; ?>
-
-                    <div class="flex flex-col gap-[20px]">
-                        <?php if( !empty($item['name']) ): ?>
-                            <h3 class="text-[24px] font-bold">
-                                <?php echo esc_html($item['name']); ?>
-                            </h3>
-                        <?php endif; ?>
-
-                        <?php if( !empty($item['job_tile']) ): ?>
-                            <h4 class="text-gray-600">
-                                <?php echo esc_html($item['job_tile']); ?>
-                            </h4>
-                        <?php endif; ?>
-                        <?php if( !empty($item['job_deescription']) ): ?>
-                            <p class="text-gray-600">
-                                <?php echo esc_html($item['job_deescription']); ?>
-                            </p>
-                        <?php endif; ?>
                     </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    <?php endwhile; ?>
 </div>
