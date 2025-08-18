954a5629c9a63264bf7d0c10113c1b56cd2d5e48
KenMirwell <kenmirwell@gmail.com>
2025-08-18 11:20:17 +0800

    update

--- about-page/mission-section.php
+++ about-page/mission-section.php
@@ -9,7 +9,7 @@
             <?php endif; ?>
             <div class="pt-[10px]">
                 <?php if (!empty($group['sub'])) : ?>
-                    <div class="text-[34px] text-[500] leading-[normal]"><?php echo esc_html($group['sub']) ?></div>
+                    <div class="text-[34px] text-[500] leading-[normal]"><?php echo ($group['sub']) ?></div>
                 <?php endif; ?>
             </div>
         </div>
