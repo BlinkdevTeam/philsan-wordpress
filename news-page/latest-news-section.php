4ca364d48ae8b53118318e6a2b4b447ed4d3e51b
KenMirwell <kenmirwell@gmail.com>
2025-08-19 09:13:19 +0800

    update

--- news-page/latest-news-section.php
+++ news-page/latest-news-section.php
@@ -16,15 +16,13 @@
     </div>
     <div class="flex gap-[50px] pt-[50px]">
         <?php if ($news->have_posts()) : ?>
-            <?php while ($news->have_posts()) : $news->the_post(); ?>
-                <?php
-                    $image = get_field("image");
-                    $description = get_field("description");
-                    $featured_news = get_field("featured_news");
-                ?>
-                <?php if($featured_news) : ?>
-                    <h2 class="text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
-                <?php else: ?>
+            <?php
+                $image = get_field("image");
+                $description = get_field("description");
+                $featured_news = get_field("featured_news");
+            ?>
+            <?php if($featured_news) : ?>
+                <?php while ($news->have_posts()) : $news->the_post(); ?>
                     <div class="p-[40px] rounded-xl bg-[#FCFCF0]">
                         <div class="flex flex-col gap-[20px]">
                             <div class="flex justify-between items-center pb-[20px]">
@@ -44,8 +42,30 @@
                             </div>
                         </div>
                     </div>
-                <?php endif; ?>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <?php while ($news->have_posts()) : $news->the_post(); ?>
+                    <div class="p-[40px]">
+                        <div class="flex flex-col gap-[20px]">
+                            <div class="flex justify-between items-center pb-[20px]">
+                                <div class="w-[60%]">
+                                    <h2 class="text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
+                                </div>
+                                <div class="">
+                                    <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl">Learn More</a>
+                                </div>
+                            </div>
+                            <div class="relative w-full h-[200px] md:h-[320px] lg:h-[450px] overflow-hidden rounded-tl-2xl rounded-br-2xl">
+                                <div class="bg-[#000000] opacity-[0.4] w-full h-full absolute top-0 left-0 z-[1]"></div>
+                                <img class="w-full h-full object-cover" src="<?php echo esc_url($image); ?>" alt="event image">
+                            </div>
+                            <div class="flex flex-col gap-[10px]">
+                                <p class="text-[34px] font-[600] leading-normal"><?php echo esc_html($description); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
             <?php wp_reset_postdata(); ?>
         <?php endif; ?>
     </div>
