958259d8c6e9ec528f718fe1d9330073d93b2108
KenMirwell <kenmirwell@gmail.com>
2025-08-19 11:29:56 +0800

    update

--- news-page/latest-news-section.php
+++ news-page/latest-news-section.php
@@ -25,7 +25,6 @@
         )
     ));
     
-    var_dump( get_post_meta( get_the_ID(), 'featured_news', true ) );
 ?>
 
 <div class="custom-container">
@@ -62,22 +61,21 @@
         <?php endif; ?>
 
         <?php if ($non_featured->have_posts()) : ?>
-            <div class="flex">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <?php while ($non_featured->have_posts()) : $non_featured->the_post(); ?>
                     <?php
                         $image       = get_field("image");
                         $description = get_field("description");
                     ?>
                     <!-- NON-FEATURED NEWS -->
-                    <div class="p-[40px]">
-                        <h2 class="text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
+                    <div class="p-[40px] bg-white rounded-lg shadow">
                         <img class="w-full h-[200px] object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($image); ?>" alt="">
-                        <p class="text-[34px] font-[600] leading-normal mt-4"><?php echo esc_html($description); ?></p>
+                        <h2 class="text-[24px] font-[600] text-[#1f773a] mb-2"><?php the_title(); ?></h2>
+                        <!-- <p class="text-[18px] leading-normal mt-4"><?php //echo esc_html($description); ?></p> -->
                     </div>
                 <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
             </div>
         <?php endif; ?>
-
     </div>
 </div>
