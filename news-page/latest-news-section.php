87840f793059923d9354871338e47437eb0ad9d9
KenMirwell <kenmirwell@gmail.com>
2025-08-19 08:26:50 +0800

    update

---
+++ news-page/latest-news-section.php
@@ -0,0 +1,10 @@
+<div class="news-hero-section h-[500px] lg:h-[720px]">
+    <div class="flex space-between h-full w-full relative y-thumbnail">
+        <div class="custom-container py-[100px] mx-auto z-[2]">
+            <div class="w-[50%] flex flex-col justify-center h-[100%]">
+                <h1 class="leading-[34px] text-[48px] xl:text-[72px] font-[700] pb-[20px] text-[#ffc200]">Our Latest News</h1>
+                <p class="text-[18px] xl:text-[24px] text-[#ffffff]">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ page-news.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+    <?php get_template_part('news-page/latest-news-section'); ?>
+<?php get_footer(); ?>
