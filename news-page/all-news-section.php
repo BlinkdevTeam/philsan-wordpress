53f36db6b08a3d74bf71277e34a990e0be9f9edc
KenMirwell <kenmirwell@gmail.com>
2025-08-19 14:23:25 +0800

    update

--- news-page/all-news-section.php
+++ news-page/all-news-section.php
@@ -4,15 +4,20 @@
     </div>
     <div class="flex flex-col gap-[50px] pt-[50px]">
         <?php if ($all_news->have_posts()) : ?>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 <?php while ($all_news->have_posts()) : $all_news->the_post(); ?>
                     <?php
                         $image       = get_field("image");
                         $description = get_field("description");
                     ?>
-                    <div class="p-[40px] bg-white rounded-lg shadow">
-                        <img class="w-full h-[200px] object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($image); ?>" alt="">
-                        <h2 class="text-[24px] font-[600] text-[#1f773a] mb-2"><?php the_title(); ?></h2>
+                    <div class="">
+                        <img class="w-full h-[300px] object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($image); ?>" alt="">
+                        <div class="pt-[20px]">
+                            <h2 class="text-[24px] leading-[normal] font-[400] text-[#000000]"><?php the_title(); ?></h2>
+                        </div>
+                        <a href="https://philsan.org/38th-annual-convention/registration/" class="flex w-fit mt-[20px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl">
+                            <span class="text-[#ffffff] text-bold text-[18px]">View More</span>
+                        </a>
                         <!-- <p class="text-[18px] leading-normal mt-4"><?php //echo esc_html($description); ?></p> -->
                     </div>
                 <?php endwhile; ?>
