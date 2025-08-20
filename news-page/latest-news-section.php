9780df778b6f44524fde23b7d66444599ce01369
KenMirwell <kenmirwell@gmail.com>
2025-08-20 10:57:51 +0800

    update

--- news-page/latest-news-section.php
+++ news-page/latest-news-section.php
@@ -21,37 +21,8 @@
                         <img class="w-full h-auto object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($image); ?>" alt="">
                     </div>
                     <div class="w-[60%]">
-                        <div class="flex justify-between">
-                            <?php
-                                if ($categories) {
-                                    $first_category = $categories[0];
-                                    $extra_count = count($categories) - 1;
-                            ?>
-                                    <div class="relative group inline-block">
-                                        <!-- First Category -->
-                                        <span class="px-2 py-1 bg-green-100 text-green-800 text-[18px] rounded">
-                                            <?php echo esc_html($first_category->name); ?>
-                                        </span>
-
-                                        <!-- Extra count -->
-                                        <?php if ($extra_count > 0): ?>
-                                            <span class="ml-1 px-2 py-1 bg-green-100 text-green-800 text-[18px] rounded">
-                                                +<?php echo $extra_count; ?>
-                                            </span>
-
-                                            <!-- Popup with all categories -->
-                                            <div class="absolute left-0 mt-2 hidden group-hover:block bg-white shadow-lg rounded p-3 text-[18px] w-max z-10">
-                                                <ul class="space-y-1">
-                                                    <?php foreach ($categories as $cat): ?>
-                                                        <li class="text-gray-800 "><?php echo esc_html($cat->name); ?></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </div>
-                                        <?php endif; ?>
-                                    </div>
-                                    <?php
-                                }
-                            ?>
+                        <div class="flex justify-between items-center">
+                            <?php include locate_template('news-page/category-element.php'); ?>
 
                             <div class="px-[20px] mb-[20px] border-[1px] border-[#000000] rounded-full w-fit">
                                 <p class="text-[18px]"><?php echo esc_html($formatted_date); ?></p>
