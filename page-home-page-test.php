7fdcbaf7061e8638eda066af0fc27aaf791ae581
KenMirwell <kenmirwell@gmail.com>
2025-08-26 15:58:06 +0800

    update

--- page-home-page-test.php
+++ page-home-page-test.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
+<div class="font-poppins">
     <?php get_template_part("front-page/hero-section"); ?>
     <?php get_template_part("front-page/figures-section"); ?>
     <?php get_template_part("front-page/about-section"); ?>
     <?php get_template_part("front-page/mission-section"); ?>
     <?php get_template_part("front-page/event-section"); ?>
     <?php get_template_part("front-page/news-section"); ?>
+</div>
 
 <?php get_footer(); ?>
