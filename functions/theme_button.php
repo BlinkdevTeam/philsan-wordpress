a134187cbe7bf523d3db2f501948541d3694277b
KenMirwell <kenmirwell@gmail.com>
2025-08-29 11:33:46 +0800

    update theme_button

--- functions/theme_button.php
+++ functions/theme_button.php
@@ -1,7 +1,7 @@
 <?php
 function theme_button($label, $url) {
     ob_start(); ?>
-        <a href="<?php echo esc_url($url); ?>" class="bg-[#ffc200] px-[20px] py-[10px] text-[#ffffff] inline-flex items-center gap-[10px] rounded-tl-[40px] rounded-br-[40px] hover:bg-[#e6ae00] transition">
+        <a href="<?php echo esc_url($url); ?>" class="bg-[#ffc200] px-[20px] py-[10px] text-[#ffffff] inline-flex items-center gap-[10px] rounded-tl-[30px] rounded-br-[30px] hover:bg-[#e6ae00] transition">
             <?php echo esc_html($label); ?>
             <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 7H17M17 7L11 1M17 7L11 13" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
