b8d99bf602914e834ee68f68417d418ccf9fa69a
KenMirwell <kenmirwell@gmail.com>
2025-08-29 09:27:41 +0800

    Change font for <p> to "inter"

--- front-page/hero-section.php
+++ front-page/hero-section.php
@@ -6,8 +6,8 @@
                 <div class="flex space-between h-full w-full relative y-thumbnail">
                     <div class="custom-container py-[100px] mx-auto z-[2]">
                         <div class="w-[50%] flex flex-col justify-center h-[100%]">
-                            <h1 class="leading-[normal] text-[48px] xl:text-[72px] font-[700] pb-[20px] text-[#ffc200]"><?php the_sub_field('hero_title'); ?></h1>
-                            <p class="text-[18px] xl:text-[24px] text-[#ffffff]"><?php the_sub_field('hero_sub'); ?></p>
+                            <h1 class="leading-[normal] text-[24px] xl:text-[48px] font-[700] pb-[20px] text-[#ffc200]"><?php the_sub_field('hero_title'); ?></h1>
+                            <p class="text-[16px] xl:text-[18px] text-[#ffffff]"><?php the_sub_field('hero_sub'); ?></p>
                         </div>
                     </div>
                     <div class="bg-gradient-to-b from-[rgba(11,83,4,0.3)] to-[rgba(11,83,4,1)] w-full h-full absolute top-0 left-0 z-[1]"></div>
