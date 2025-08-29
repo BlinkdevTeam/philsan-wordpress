6c59edf11bd8a5d544d7e883eac1b68f1c1cbf36
KenMirwell <kenmirwell@gmail.com>
2025-08-29 12:02:20 +0800

    fixing missons-section for  frontpage

--- front-page/mission-section.php
+++ front-page/mission-section.php
@@ -6,18 +6,18 @@
         <div class="flex custom-container mx-auto gap-[50px]">
             <div class="w-[40%]">
                 <?php if (!empty($group['title'])) : ?>
-                    <h2 class="text-[24px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
+                    <h2 class="text-[18px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
                 <?php endif; ?>
                 <div class="pt-[10px]">
                     <?php if (!empty($group['description'])) : ?>
-                        <p class="text-[34px] text-[500] leading-[34px]"><?php echo esc_html($group['description']) ?></p>
+                        <p class="text-[24px] font-[500] leading-[34px]"><?php echo esc_html($group['description']) ?></p>
                     <?php endif; ?>
                 </div>
-                <div class="pt-[40px]">
-                    <?php if (!empty($group['button_title'])) : ?>
-                        <a href="https://philsan.org/38th-annual-convention/registration/" class="text-[#ffffff] text-bold text-[18px] bg-[#FFC200] py-[15px] px-[25px] rounded-tl-2xl rounded-br-2xl"><?php echo esc_html($group['button_title']) ?></a>
-                    <?php endif; ?>
-                </div>
+                <?php if (!empty($group['button_title'])) : ?>
+                    <div class="flex pt-[30px]">
+                        <?php echo theme_button(esc_html($group['button_title']), "/"); ?>
+                    </div>
+                <?php endif; ?>
             </div>
             <div class="w-[60%]">
                 <div class="w-[100%] image-container">
