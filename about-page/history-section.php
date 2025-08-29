3fb01080179a5d301d00a5366db7fc2d72402053
KenMirwell <kenmirwell@gmail.com>
2025-08-29 16:18:54 +0800

    developing history section for abouts page

--- about-page/history-section.php
+++ about-page/history-section.php
@@ -1,6 +1,6 @@
 <div class="history-section custom-container mx-auto px-6 py-12">
-    <h2 class="text-[42px] font-bold text-[#1F773A] mb-[10px] text-center">Our History</h2>
-    <p class="text-[18px] font-bold text-[#1F773A] mb-8 text-center">( a glance at the past )</p>
+    <h2 class="text-[42px] font-bold text-[#1F773A] text-center">Our History</h2>
+    <p class="text-[18px] font-[600] text-[#1F773A] mb-8 text-center">( a glance at the past )</p>
 
     <div class="space-y-12">
         <?php if ( have_rows('history_repeater') ): ?>
@@ -11,18 +11,30 @@
                         <?php the_sub_field("year"); // e.g. "1988" ?>
                     </h2>
 
-                    <?php if ( have_rows('year_group') ): ?>
-                        <ul class="space-y-3 border-l-2 border-[##1F773A] pl-6">
-                            <?php while ( have_rows('year_group') ): the_row(); ?>
-                                <li>
-                                    <span class="font-bold">
-                                        <?php the_sub_field("title"); // e.g. "June 28" ?>
-                                    </span> 
-                                    <?php the_sub_field("description"); ?>
-                                </li>
-                            <?php endwhile; ?>
-                        </ul>
-                    <?php endif; ?>
+                    <div class="flex">
+                        <div class="w-[100%] image-container">
+                            <div class="h-[200px] md:h-[250px] lg:h-[300px] rounded-2xl overflow-hidden">
+                                <img 
+                                    class="w-full h-full object-cover filter grayscale hover:grayscale-0 transition duration-300" 
+                                    src="<?php echo get_sub_field('image'); ?>" 
+                                    alt="two col image"
+                                >
+                            </div>
+                        </div>
+
+                        <?php if ( have_rows('year_group') ): ?>
+                            <ul class="space-y-3 border-l-2 border-[#1F773A] pl-6">
+                                <?php while ( have_rows('year_group') ): the_row(); ?>
+                                    <li>
+                                        <span class="font-bold">
+                                            <?php the_sub_field("title"); // e.g. "June 28" ?>
+                                        </span> 
+                                        <?php the_sub_field("description"); ?>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                        <?php endif; ?>
+                    </div>
                 </div>
 
             <?php endwhile; ?>
