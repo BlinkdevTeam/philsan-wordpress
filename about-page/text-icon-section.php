21ed3a4c51b13011594cd94fc552287fe755b330
KenMirwell <kenmirwell@gmail.com>
2025-08-29 15:05:05 +0800

    update

--- about-page/text-icon-section.php
+++ about-page/text-icon-section.php
@@ -18,7 +18,7 @@
     <?php if( !empty($group['text_icon_section']) && is_array($group['text_icon_section']) ): ?>
         <div class="flex gap-[20px] justify-center">
             <?php foreach( $group['text_icon_section'] as $item ): ?>
-                <div class="flex flex-col gap-[20px] items-start w-[25%]">
+                <div class="flex flex-col gap-[20px] items-start w-[25%] rounded-md p-[20px] shadow-md bg-white">
                     <?php if( !empty($item['icon']) ): ?>
                         <div class="bg-[#CBF9B6] p-[10px] rounded-xl">
                             <img src="<?php echo esc_url($item['icon']['url']); ?>" alt="icon image" class="w-10 h-10 object-contain" />
