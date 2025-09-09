fa140db4796a6ac1768d86aeb96815df0edba121
KenMirwell <kenmirwell@gmail.com>
2025-09-09 15:58:52 +0800

    building contact page

--- contact-page/form-component.php
+++ contact-page/form-component.php
@@ -1,7 +1,9 @@
 <?php $form_shortcode = get_field('contact_form'); ?>
 
-<div>
-    <?php if($form_shortcode): ?>
-        <?php echo do_shortcode($form_shortcode); ?>
-    <?php endif; ?>
+<div class="w-[500px] mx-auto">
+    <div class="p-[50px] rounded-2xl bg-[#FCFCF0]">
+        <?php if($form_shortcode): ?>
+            <?php echo do_shortcode($form_shortcode); ?>
+        <?php endif; ?>
+    </div>
 </div>
