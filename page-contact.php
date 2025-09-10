42ee5d92826969f2a630f383ff8a69607533e099
KenMirwell <kenmirwell@gmail.com>
2025-09-10 09:53:06 +0800

    update faq section

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
     <?php get_template_part("contact-page/hero-section"); ?>
     <?php get_template_part("contact-page/form-component"); ?>
+    <?php get_template_part("contact-page/faqs-section"); ?>
 <?php get_footer(); ?>
