b095bdf82bbda0a7dcc5bc452ecbd2ab0ff8a374
KenMirwell <kenmirwell@gmail.com>
2025-09-11 12:18:00 +0800

    update

--- main-pages-sections/news-page/featured-news.php
+++ main-pages-sections/news-page/featured-news.php
@@ -3,16 +3,16 @@
         <h2 class="text-[42px] font-[700] pb-[20px] text-[#1F773A]">Featured News</h2>
     </div>
     <?php if ($featured->have_posts()) : ?>
-        <div class="swiper featuredNews mt-[-200px]">
+        <div class="swiper featuredNews mt-[-200px] h-[600px]"> <!-- fixed height -->
             <div class="swiper-wrapper">
                 <?php while ($featured->have_posts()) : $featured->the_post(); ?>
                     <?php
                         $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-                        $description = get_the_content();
-                        $date        = get_field("date");
-                        $categories  = get_the_terms(get_the_ID(), 'category-filters');
+                        $description        = get_the_content();
+                        $date               = get_field("date");
+                        $categories         = get_the_terms(get_the_ID(), 'category-filters');
 
-                        // Reformat the date
+                        // Format date
                         if ($date) {
                             $formatted_date = DateTime::createFromFormat('m/d/Y', $date)->format('F j, Y');
                         } else {
@@ -20,24 +20,29 @@
                         }
                     ?>
                     <!-- FEATURED NEWS -->
-                    <div class="flex gap-[20px] p-[40px] rounded-xl bg-[#FCFCF0] swiper-slide">
-                        <div class="w-[40%]">
-                            <?php if ($featured_image_url): ?>
-                                <img class="w-full h-auto object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($featured_image_url); ?>" alt="">
-                            <?php endif; ?>
-                        </div>
-                        <div class="flex flex-col gap-[10px] w-[60%]">
-                            <div class="flex justify-between items-center">
-                                <?php include locate_template('main-pages-sections/news-page/category-element.php'); ?>
-
-                                <div class="px-[20px] border-[1px] border-[#000000] rounded-full w-fit">
-                                    <p class="text-[16px]"><?php echo esc_html($formatted_date); ?></p>
-                                </div>
+                    <div class="swiper-slide h-full flex">
+                        <div class="flex gap-[20px] p-[40px] rounded-xl bg-[#FCFCF0] w-full">
+                            <div class="w-[40%]">
+                                <?php if ($featured_image_url): ?>
+                                    <img class="w-full h-auto object-cover rounded-tl-2xl rounded-br-2xl" 
+                                        src="<?php echo esc_url($featured_image_url); ?>" 
+                                        alt="">
+                                <?php endif; ?>
                             </div>
-                            <h2 class="text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?> </h2>
-                            <p class="text-[16px] font-[400]"><?php echo esc_html(wp_strip_all_tags($description)); ?></p>
-                            <div class="flex pt-[30px]">
-                                <?php echo theme_button("View More", get_permalink()); ?>
+                            <div class="flex flex-col gap-[10px] w-[60%]">
+                                <div class="flex justify-between items-center">
+                                    <?php include locate_template('main-pages-sections/news-page/category-element.php'); ?>
+                                    <div class="px-[20px] border border-black rounded-full w-fit">
+                                        <p class="text-[16px]"><?php echo esc_html($formatted_date); ?></p>
+                                    </div>
+                                </div>
+                                <h2 class="text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
+                                <p class="text-[16px] font-[400]">
+                                    <?php echo esc_html(wp_strip_all_tags($description)); ?>
+                                </p>
+                                <div class="flex pt-[30px]">
+                                    <?php echo theme_button("View More", get_permalink()); ?>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -45,9 +50,10 @@
                 <?php wp_reset_postdata(); ?>
             </div>
 
-            <!-- Optional pagination if you want -->
+            <!-- pagination -->
             <div class="swiper-pagination"></div>
         </div>
+
 
     <?php endif; ?>
 </div>
