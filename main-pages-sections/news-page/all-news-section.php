31988ec61cde5169fe7020b0d7a001fb06aef72e
KenMirwell <kenmirwell@gmail.com>
2025-09-11 11:34:21 +0800

    update

--- main-pages-sections/news-page/all-news-section.php
+++ main-pages-sections/news-page/all-news-section.php
@@ -11,9 +11,12 @@
                         $description = get_field("description");
                         $date        = get_field("date");
                         $categories = get_the_terms( get_the_ID(), 'category-filters' );
-                        
-                        $desc_limit = 40;
+                        $title = get_the_title();
+
+                        $title_limit = 60;
+                        $desc_limit = 60;
                         $desc_trimmed = mb_strimwidth($description, 0, $desc_limit, "..."); // Trim it properly
+                        $title_trimmed = mb_strimwidth($description, 0, $title_limit, "...");
                         // Reformat the date
                         if ($date) {
                             $formatted_date = DateTime::createFromFormat('m/d/Y', $date)->format('F j, Y');
@@ -29,7 +32,7 @@
                                 <p class="text-[14px]"><?php echo esc_html($formatted_date); ?></p>
                             </div>
                             <h2 class="text-[18px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
-                            <p class="text-[14px] font-[400]"><?php echo esc_html($desc_trimmed); ?></p>
+                            <p class="text-[14px] font-[400]"><?php echo esc_html($title_trimmed); ?></p>
                         </div>
                         <div class="flex pt-[30px]">
                             <?php echo theme_button("View More", "/"); ?>
