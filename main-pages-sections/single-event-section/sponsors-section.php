5d726b4703454f2c38d3e9e014469579aa4f7b88
KenMirwell <kenmirwell@gmail.com>
2025-09-15 15:54:43 +0800

    update

--- main-pages-sections/single-event-section/sponsors-section.php
+++ main-pages-sections/single-event-section/sponsors-section.php
@@ -1,23 +1,12 @@
 <?php 
     $group = get_field('sponsor_section');
 ?>
-<div class="sponsors-section custom-container py-[50px]">
-    <div class="flex flex-col justify-center items-center">
-        <?php if (!empty($group['title'])) : ?>
-            <h2 class="text-[42px] text-[#1F773A] font-[700]"><?php echo esc_html($group['title']) ?></h2>
-        <?php endif; ?>
-
-        <?php if (!empty($group['sub'])) : ?>
-            <div class="pt-[10px]">
-                <div class="text-[24px] leading-[34px] flex flex-col gap-[20px]">
-                    <?php echo wp_kses_post($group['sub']); ?>
-                </div>
-            </div>
-        <?php endif; ?>
-    </div>
-    
-    <div class="pt-[50px]">
-        <?php if ( !empty($group['sponsor_repeater']) ) : ?> 
+<?php if ( !empty($group['sponsor_repeater']) ) : ?> 
+    <div class="sponsors-section custom-container py-[50px]">
+        <div class="flex flex-col justify-center items-center">
+            <h2 class="text-[24px] text-[#1F773A] font-[700]">Event Sponsors</h2>
+        </div>
+        <div class="pt-[50px]">
             <?php foreach ( $group['sponsor_repeater'] as $row ) : ?>
                     <?php 
                         $image_url   = $row['logo'];
@@ -31,6 +20,6 @@
                     </div>
                 <?php endforeach; ?> 
             </div>
-        <?php endif; ?>
+        </div>
     </div>
-</div>
+ <?php endif; ?>
--- single-event.php
+++ single-event.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
     <?php get_template_part("main-pages-sections/single-event-section/hero-section"); ?>
     <?php get_template_part("main-pages-sections/single-event-section/speakers-section"); ?>
+    <?php get_template_part("main-pages-sections/single-event-section/sponsors-section"); ?>
 <?php get_footer(); ?>
