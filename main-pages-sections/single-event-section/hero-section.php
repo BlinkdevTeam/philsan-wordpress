42ed2aa343f98d2f7ca9d8374bad11f217ae6a1c
KenMirwell <kenmirwell@gmail.com>
2025-09-15 13:50:42 +0800

    update

--- main-pages-sections/single-event-section/hero-section.php
+++ main-pages-sections/single-event-section/hero-section.php
@@ -7,8 +7,8 @@
 <div class="single-event-hero-section h-[580px]">
     <div class="flex space-between h-full w-full relative y-thumbnail">
         <div class="custom-container py-[100px] mx-auto z-[2]">
-            <div class="flex gap-[50px]">
-                <div class="w-[40%] flex flex-col justify-center text-left h-[100%]">
+            <div class="flex justify-between gap-[50px]">
+                <div class="w-[50%] flex flex-col justify-center text-left h-[100%]">
                     <h1 class="text-[48px] font-[700] pb-[20px] text-[#ffffff]"><?php the_title(); ?></h1>
                     <p class="text-[18px] text-[#ffffff]"><?php echo $description ?></p>
                 </div>
@@ -17,6 +17,6 @@
                 </div>
             </div>
         </div>
-        <div class="bg-gradient-to-b from-[#051e22] to-[#5a944d] w-full h-full absolute top-0 left-0 z-[1]"></div
+        <div class="bg-gradient-to-b from-[#1f773a] to-[#5a944d] w-full h-full absolute top-0 left-0 z-[1]"></div>
     </div>
 </div>
