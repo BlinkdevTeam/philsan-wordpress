7a638dfb8cc819ff5508e36e17cfc418f05103af
KenMirwell <kenmirwell@gmail.com>
2025-09-15 13:25:51 +0800

    update

--- main-pages-sections/single-event-section/hero-section.php
+++ main-pages-sections/single-event-section/hero-section.php
@@ -60,7 +60,7 @@
                     </div>
                 </div>
                 <div class="bg-gradient-to-b from-[rgba(11,83,4,0.3)] to-[rgba(11,83,4,1)] w-full h-full absolute top-0 left-0 z-[1]"></div>
-                <img class="absolute w-full h-full object-cover" src="<?php echo get_sub_field('hero_image'); ?>" alt="hero-image">
+                <img class="absolute w-full h-full object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
             </div>
         <?php endwhile; ?>
     <?php endif; ?>
