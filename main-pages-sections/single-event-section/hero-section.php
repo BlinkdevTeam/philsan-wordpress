15241375992fae8e5889a25230653b3689451d51
KenMirwell <kenmirwell@gmail.com>
2025-09-15 13:37:03 +0800

    update

--- main-pages-sections/single-event-section/hero-section.php
+++ main-pages-sections/single-event-section/hero-section.php
@@ -3,13 +3,16 @@
 <div class="single-event-hero-section h-[580px]">
     <div class="flex space-between h-full w-full relative y-thumbnail">
         <div class="custom-container py-[100px] mx-auto z-[2]">
-            <div class="w-[100%] flex flex-col justify-center text-center h-[100%]">
-                <h1 class="text-[48px] font-[700] pb-[20px] text-[#ffffff]"><?php the_title(); ?></h1>
-                <p class="text-[18px] text-[#ffffff]"><?php the_content(); ?></p>
+            <div class="flex gap-[50px]">
+                <div class="w-[60%] flex flex-col justify-center text-left h-[100%]">
+                    <h1 class="text-[48px] font-[700] pb-[20px] text-[#ffffff]"><?php the_title(); ?></h1>
+                    <p class="text-[18px] text-[#ffffff]"><?php the_content(); ?></p>
+                </div>
+                <div class="w=[40%] rounded-xl">
+                    <img class="absolute w-full h-full object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
+                </div>
             </div>
         </div>
         <div class="bg-[#1f773a] w-full h-full absolute top-0 left-0 z-[1]"></div>
-        <!-- <div class="bg-gradient-to-b from-[rgba(11,83,4,0.3)] to-[rgba(11,83,4,1)] w-full h-full absolute top-0 left-0 z-[1]"></div>
-        <img class="absolute w-full h-full object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image"> -->
     </div>
 </div>
