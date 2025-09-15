fc0fc50aebcca0de70256c996ccbbe237b0ed924
KenMirwell <kenmirwell@gmail.com>
2025-09-15 13:42:08 +0800

    update

--- main-pages-sections/single-event-section/hero-section.php
+++ main-pages-sections/single-event-section/hero-section.php
@@ -1,15 +1,19 @@
-<?php $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
+<?php 
+    $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
+    $description = get_the_content();
+    $description = wp_strip_all_tags($description);    
+?>
 
 <div class="single-event-hero-section h-[580px]">
     <div class="flex space-between h-full w-full relative y-thumbnail">
         <div class="custom-container py-[100px] mx-auto z-[2]">
             <div class="flex gap-[50px]">
-                <div class="w-[60%] flex flex-col justify-center text-left h-[100%]">
+                <div class="w-[40%] flex flex-col justify-center text-left h-[100%]">
                     <h1 class="text-[48px] font-[700] pb-[20px] text-[#ffffff]"><?php the_title(); ?></h1>
-                    <p class="text-[18px] text-[#ffffff]"><?php the_content(); ?></p>
+                    <p class="text-[18px] text-[#ffffff]"><?php echo $description ?></p>
                 </div>
-                <div class="w=[40%] rounded-xl">
-                    <img class="w-[500px] h-[600px] object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
+                <div class="w=[40%] rounded-xl overflow-hidden">
+                    <img class="w-[500px] h-[400px] object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
                 </div>
             </div>
         </div>
