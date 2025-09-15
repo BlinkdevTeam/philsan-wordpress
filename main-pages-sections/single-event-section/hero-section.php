7116e4d4a74b7d15fed2da6d7c8cbd929fb96ecf
KenMirwell <kenmirwell@gmail.com>
2025-09-15 14:00:23 +0800

    update

--- main-pages-sections/single-event-section/hero-section.php
+++ main-pages-sections/single-event-section/hero-section.php
@@ -2,21 +2,39 @@
     $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
     $description = get_the_content();
     $description = wp_strip_all_tags($description);    
+    $date        = get_field("date");
+    $categories = get_the_terms( get_the_ID(), 'category-filters' );
+    $button_link = get_field("button_link");
+    $permalink = get_permalink();
+    
+    $desc_limit = 200;
+    $desc_trimmed = mb_strimwidth($description, 0, $desc_limit, "..."); // Trim it properly
+    // Reformat the date
+    if ($date) {
+        $formatted_date = DateTime::createFromFormat('m/d/Y', $date)->format('F j, Y');
+        $date_obj = new DateTime($date);
+
+        // Format parts separately
+        $month = $date_obj->format('M'); // short month name: Jan, Feb, Mar
+        $year  = $date_obj->format('Y'); // year: 2025
+    } else {
+        $formatted_date = '';
+    }
 ?>
 
 <div class="single-event-hero-section h-[580px]">
     <div class="flex space-between h-full w-full relative y-thumbnail">
         <div class="custom-container py-[100px] mx-auto z-[2]">
-            <div class="flex justify-between gap-[50px]">
-                <div class="w-[50%] flex flex-col justify-center text-left h-[100%]">
+            <div class="flex justify-between items-center gap-[50px]">
+                <div class="w-[100%] flex flex-col justify-center text-center h-[100%]">
                     <h1 class="text-[48px] font-[700] pb-[20px] text-[#ffffff]"><?php the_title(); ?></h1>
-                    <p class="text-[18px] text-[#ffffff]"><?php echo $description ?></p>
-                </div>
-                <div class="w=[40%] rounded-xl overflow-hidden">
-                    <img class="w-[500px] h-[400px] object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
+                    <div class="w-[70%]">
+                        <p class="text-[18px] text-[#ffffff]"><?php echo $description ?></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="bg-gradient-to-b from-[#1f773a] to-[#5a944d] w-full h-full absolute top-0 left-0 z-[1]"></div>
+        <div class="bg-gradient-to-b from-[#1f773a] to-[#6fbd5d] w-full h-full absolute top-0 left-0 z-[1]"></div>
+        <img class="w-full h-full object-cover" src="<?php echo $featured_image_url ?>" alt="hero-image">
     </div>
 </div>
