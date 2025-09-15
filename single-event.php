d7d1039773a2d791b53f407a66d0aee8b032c89d
KenMirwell <kenmirwell@gmail.com>
2025-09-15 13:23:32 +0800

    update

--- single-event.php
+++ single-event.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?>
-    <?php get_template_part("main-pages-sections/single-events-section/hero-section"); ?>
+    <?php get_template_part("main-pages-sections/single-event-section/hero-section"); ?>
 <?php get_footer(); ?>
