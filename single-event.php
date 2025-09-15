a504a8764ee10a3d9556cc69fe314982053ebfbe
KenMirwell <kenmirwell@gmail.com>
2025-09-15 15:25:32 +0800

    update

--- single-event.php
+++ single-event.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
     <?php get_template_part("main-pages-sections/single-event-section/hero-section"); ?>
-    <?php get_template_part("main-pages-sections/single-event-section/split-section"); ?>
+    <?php get_template_part("main-pages-sections/single-event-section/speaker-position"); ?>
 <?php get_footer(); ?>
