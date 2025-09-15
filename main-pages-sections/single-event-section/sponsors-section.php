bdbb2698d4ae7cdfbfac0e40c58a8d0a88c0dddd
KenMirwell <kenmirwell@gmail.com>
2025-09-15 16:25:14 +0800

    update

--- main-pages-sections/single-event-section/sponsors-section.php
+++ main-pages-sections/single-event-section/sponsors-section.php
@@ -6,18 +6,25 @@
         <div class="flex flex-col justify-center items-center">
             <h2 class="text-[24px] text-[#1F773A] font-[700]">Event Sponsors</h2>
         </div>
-        <div class="pt-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-[50px] mt-8">
-            <?php foreach ( $group['sponsor_repeater'] as $row ) : ?>
-                    <?php 
-                        $image_url   = $row['logo'];
-                        $name        = $row['name'];
-                        $description = $row['description'];
-                    ?>
-                    <div class="">
-                        <img class="w-full h-auto object-cover" src="<?php echo esc_url($image_url); ?>" alt="sponsor logo">
+        <div class="pt-[50px]">
+            <?php if ( !empty($group['sponsor_repeater']) ) : ?> 
+                <div class="swiper sponsorSwiper w-[100%]">
+                    <div class="swiper-wrapper items-center">
+                        <?php foreach ( $group['sponsor_repeater'] as $row ) : ?>
+                            <?php 
+                                $image_url   = $row['logo'];
+                                $name        = $row['name'];
+                                $description = $row['description'];
+                            ?>
+                            <div class="swiper-slide justify-center">
+                                <div class="">
+                                    <img class="w-full h-auto object-cover" src="<?php echo esc_url($image_url); ?>" alt="sponsor logo">
+                                </div>
+                            </div>
+                        <?php endforeach; ?> 
                     </div>
-                <?php endforeach; ?> 
-            </div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
  <?php endif; ?>
