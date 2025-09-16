9c75edf324ae0b89b9542596afbeb33ada5acb17
KenMirwell <kenmirwell@gmail.com>
2025-09-16 14:55:34 +0800

    update

--- main-pages-sections/news-page/featured-news.php
+++ main-pages-sections/news-page/featured-news.php
@@ -5,7 +5,7 @@
     <?php if ($featured->have_posts()) : ?>
         <!-- DESKTOP -->
         <div class="hidden md:block">
-            <div class="swiper featuredNews mt-[-200px] w-[100%]">
+            <div class="swiper featuredNews mt-[-200px] w-[100%] h-[600px]">
                 <div class="swiper-wrapper">
                     <?php while ($featured->have_posts()) : $featured->the_post(); ?>
                         <?php
@@ -69,12 +69,14 @@
 
         <!-- MOBILE -->
         <div class="md:hidden">
-            <div class="swiper featuredNews mt-[-200px] w-[100%]">
+            <div class="swiper featuredNews mt-[-200px] w-[100%] h-[800px]">
                 <div class="swiper-wrapper">
                     <?php while ($featured->have_posts()) : $featured->the_post(); ?>
                         <?php
                             $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
                             $description = get_the_content();
+                            $desc_limit = 200;
+                            $desc_trimmed = mb_strimwidth($description, 0, $desc_limit, "..."); // Trim it properly
                             $date        = get_field("date");
                             $categories = get_the_terms( get_the_ID(), 'category-filters' );
                             $button_link = get_field("button_link");
@@ -90,27 +92,26 @@
                         ?>
                         <!-- FEATURED NEWS -->
                         <div class="swiper-slide justify-start">
-                            <div class="flex gap-[20px] p-[40px] rounded-xl bg-[#FCFCF0]">
+                            <div class="flex gap-[20px] px-[20px] py-[40px] rounded-xl bg-[#FCFCF0]">
                                 <div class="flex flex-col gap-[10px] w-[100%]">
-                                    <div class="flex justify-between items-center">
-                                        <?php include locate_template('main-pages-sections/news-page/category-element.php'); ?>
-
-                                        <div class="px-[20px] border-[1px] border-[#000000] rounded-full w-fit">
-                                            <p class="text-[16px]"><?php echo esc_html($formatted_date); ?></p>
+                                    <div class="flex flex-col items-baseline justify-between items-center">
+                                         <div class="px-[20px] border-[1px] border-[#000000] rounded-full w-fit">
+                                            <p class="text-[12px]"><?php echo esc_html($formatted_date); ?></p>
                                         </div>
+                                        <?php include locate_template('main-pages-sections/news-page/category-element.php'); ?>
                                     </div>
                                     <h2 class="text-left text-[18px] md:text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?> </h2>
-                                    <div class="w-[100%]">
+                                    <div class="w-[100%] h-[200px]">
                                         <img class="w-full h-full object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($featured_image_url); ?>" alt="">
                                     </div>
-                                    <p class="text-left text-[14px] md:text-[16px] font-[400]"><?php echo esc_html($description); ?></p>
+                                    <p class="text-left text-[14px] md:text-[16px] font-[400]"><?php echo esc_html($desc_trimmed); ?></p>
                                     <?php if (have_rows('social_media')) : ?>
                                         <div class="flex gap-[20px] pt-[20px]">
                                             <?php while (have_rows('social_media')) : the_row(); ?>
                                                 <!-- Loop through each row in the 'about_description' repeater -->
                                             <a href="<?php echo esc_url(get_sub_field('socmed_link')); ?>" class="cursor-pointer p-[8px] rounded-full bg-[#e6fcdc]">
-                                                    <img class="w-[20px] h-[20px] object-cover" src="<?php echo esc_url(get_sub_field('socmed_icon')); ?>" alt="">
-                                                </a>
+                                                <img class="w-[20px] h-[20px] object-cover" src="<?php echo esc_url(get_sub_field('socmed_icon')); ?>" alt="">
+                                            </a>
                                             <?php endwhile; ?>
                                         </div>
                                     <?php endif; ?> 
