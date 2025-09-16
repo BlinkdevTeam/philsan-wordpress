a00dbe06dbda3ce19da4050c2027fd0897e013ac
KenMirwell <kenmirwell@gmail.com>
2025-09-16 15:36:58 +0800

    update

--- main-pages-sections/events-page/featured-events.php
+++ main-pages-sections/events-page/featured-events.php
@@ -66,4 +66,67 @@
     </div>
 
     <!-- MOBILE -->
+     <div class="mt-[-200px] custom-container">
+        <div class="swiper featuredEvents mt-[-200px] w-[100%]">
+            <div class="swiper-wrapper">
+                <?php while ($featured->have_posts()) : $featured->the_post(); ?>
+                    <?php
+                        $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                        $description = get_the_content();
+                        $description = wp_strip_all_tags($description);
+                        $desc_limit = 200;
+                        $desc_trimmed = mb_strimwidth($description, 0, $desc_limit, "..."); // Trim it properly
+                        $date        = get_field("date");
+                        $categories = get_the_terms( get_the_ID(), 'category-filters' );
+                        $button_link = get_field("button_link");
+                        $permalink = get_permalink();
+
+                        // Reformat the date
+                        if ($date) {
+                            $formatted_date = DateTime::createFromFormat('m/d/Y', $date)->format('F j, Y');
+                        } else {
+                            $formatted_date = '';
+                        }
+                    ?>
+                    <!-- FEATURED EVENTS MOBILE -->
+                    <div class="swiper-slide">
+                        <div class="flex gap-[20px] py-[50px] px-[40px] rounded-xl bg-[#FCFCF0]">
+                            <div class="flex flex-col gap-[10px] w-[100%]">
+                                <div class="flex justify-between items-center">
+                                    <?php include locate_template('main-pages-sections/events-page/category-element.php'); ?>
+                                    <div class="px-[20px] border-[1px] border-[#000000] rounded-full w-fit">
+                                        <p class="text-[14px] md:text-[16px]"><?php echo esc_html($formatted_date); ?></p>
+                                    </div>
+                                </div>
+                                <h2 class="text-[18px] md:text-[24px] text-left font-[600] text-[#1f773a] text-left"><?php the_title(); ?> </h2>
+                                <div class="w-[100%]">
+                                    <img class="w-full h-[300px] object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($featured_image_url); ?>" alt="">
+                                </div>
+                                <div class="text-[14px] md:text-[18px] text-left font-[400]"><?php echo esc_html($desc_trimmed); ?></div>
+                                <?php if (have_rows('social_media')) : ?>
+                                    <div class="flex gap-[20px] pt-[20px]">
+                                        <?php while (have_rows('social_media')) : the_row(); ?>
+                                            <!-- Loop through each row in the 'about_description' repeater -->
+                                        <a href="<?php echo esc_url(get_sub_field('socmed_link')); ?>" class="cursor-pointer p-[8px] rounded-full bg-[#e6fcdc]">
+                                                <img class="w-[20px] h-[20px] object-cover" src="<?php echo esc_url(get_sub_field('socmed_icon')); ?>" alt="">
+                                            </a>
+                                        <?php endwhile; ?>
+                                    </div>
+                                <?php endif; ?> 
+                                <div class="flex pt-[30px]">
+                                    <?php if($button_link) : ?>
+                                        <?php echo theme_button("View More", $button_link); ?>
+                                    <?php else : ?>
+                                        <?php echo theme_button("View More", $permalink); ?>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
 <?php endif; ?>
--- main-pages-sections/events-page/past-events.php
+++ main-pages-sections/events-page/past-events.php
@@ -1,7 +1,9 @@
 <?php if ($past_events->have_posts()) : ?>
     <div class="py-[100px]">
+
+        <!-- DESKTOP -->
         <div class="flex flex-col gap-[50px]">
-            <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-[30px]">
+            <div class="grid grid-cols-1 gap-[30px]">
                 <?php while ($past_events->have_posts()) : $past_events->the_post(); ?>
                     <?php
                         $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
@@ -35,7 +37,6 @@
                         </div>
                         <div class="w-[50%] pl-[20px]">
                             <div class="pt-[20px] w-[80%]">
-                                <?php include locate_template('main-pages-sections/events-page/category-element.php'); ?>
                                 <div class="px-[20px] mt-[10px] mb-[20px] border-[1px] border-[#000000] rounded-full w-fit">
                                     <p class="text-[14px]"><?php echo esc_html($formatted_date); ?></p>
                                 </div>
@@ -69,17 +70,84 @@
                 <?php wp_reset_postdata(); ?>
             </div>
         </div>
+
+        <!-- MOBILE -->
+         <div class="flex flex-col gap-[50px]">
+            <div class="grid grid-cols-1 gap-[30px]">
+                <?php while ($past_events->have_posts()) : $past_events->the_post(); ?>
+                    <?php
+                        $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                        $description = get_the_content();
+                        $description = wp_strip_all_tags($description);
+                        $date        = get_field("date");
+                        $categories = get_the_terms( get_the_ID(), 'category-filters' );
+                        $button_link = get_field("button_link");
+                        $permalink = get_permalink();
+                        
+                        $desc_limit = 200;
+                        $desc_trimmed = mb_strimwidth($description, 0, $desc_limit, "..."); // Trim it properly
+                        // Reformat the date
+                        if ($date) {
+                            $formatted_date = DateTime::createFromFormat('m/d/Y', $date)->format('F j, Y');
+                            $date_obj = new DateTime($date);
+
+                            // Format parts separately
+                            $month = $date_obj->format('M'); // short month name: Jan, Feb, Mar
+                            $year  = $date_obj->format('Y'); // year: 2025
+                        } else {
+                            $formatted_date = '';
+                        }
+                    ?>
+                    <div class="flex justify-between gap-[20px] border-b-[1px] border-[#cccccc] pb-[30px]">
+                        <div class="w-[50%] pl-[20px]">
+                            <div class="pt-[20px] w-[100%]">
+                                <div class="px-[20px] mt-[10px] mb-[20px] border-[1px] border-[#000000] rounded-full w-fit">
+                                    <p class="text-[14px]"><?php echo esc_html($formatted_date); ?></p>
+                                </div>
+                                <h2 class="text-[18px] md:text-[24px] font-[600] text-[#1f773a]"><?php the_title(); ?></h2>
+                                 <div class="w-[100%] py-[5px]">
+                                    <img class="w-full h-[200px] object-cover rounded-tl-2xl rounded-br-2xl" src="<?php echo esc_url($featured_image_url); ?>" alt="">
+                                </div>
+                                <div class="text-[16px] font-[400]"><?php echo esc_html($desc_trimmed); ?></div>
+                                <?php if (have_rows('social_media')) : ?>
+                                    <div class="flex gap-[20px] pt-[20px]">
+                                        <?php while (have_rows('social_media')) : the_row(); ?>
+                                            <!-- Loop through each row in the 'about_description' repeater -->
+                                        <a href="<?php echo esc_url(get_sub_field('socmed_link')); ?>" class="cursor-pointer p-[8px] rounded-full bg-[#dfdfdf]">
+                                                <img class="w-[20px] h-[20px] object-cover" src="<?php echo esc_url(get_sub_field('socmed_icon')); ?>" alt="">
+                                            </a>
+                                        <?php endwhile; ?>
+                                    </div>
+                                <?php endif; ?> 
+                            </div>
+                            <div class="flex pt-[30px]">
+                                <?php if($button_link) : ?>
+                                    <?php echo theme_button("View More", $button_link); ?>
+                                <?php else : ?>
+                                    <?php echo theme_button("View More", $permalink); ?>
+                                <?php endif; ?>
+                            </div>
+                            <!-- <p class="text-[18px] leading-normal mt-4"><?php //echo esc_html($description); ?></p> -->
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+        </div>
+        <!-- END OF MOBILE -->
+
+
         <div class="flex justify-center space-x-2 mt-6">
-        <?php
-            echo paginate_links(array(
-                'total'   => $past_events->max_num_pages,
-                'current' => $paged,
-                'prev_text' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">«</span>',
-                'next_text' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">»</span>',
-                'before_page_number' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">',
-                'after_page_number'  => '</span>',
-            ));
-            ?>
+            <?php
+                echo paginate_links(array(
+                    'total'   => $past_events->max_num_pages,
+                    'current' => $paged,
+                    'prev_text' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">«</span>',
+                    'next_text' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">»</span>',
+                    'before_page_number' => '<span class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">',
+                    'after_page_number'  => '</span>',
+                ));
+                ?>
         </div>
     </div>
 <?php endif; ?>
