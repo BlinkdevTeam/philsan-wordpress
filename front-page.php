c80f03f99828cd406c22ff4c4f8c0df83be6061c
KenMirwell <kenmirwell@gmail.com>
2025-10-17 12:24:29 +0800

    update

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<?php get_template_part("main-pages-sections/front-page/hero-section"); ?>
+<?php //get_template_part("main-pages-sections/front-page/hero-section"); ?>
 <?php //get_template_part("main-pages-sections/front-page/figures-section"); ?>
-<?php get_template_part("main-pages-sections/front-page/about-section"); ?>
-<?php get_template_part("main-pages-sections/front-page/mission-section"); ?>
-<?php get_template_part("main-pages-sections/front-page/event-section"); ?>
-<?php get_template_part("main-pages-sections/front-page/news-section"); ?>
+<?php //get_template_part("main-pages-sections/front-page/about-section"); ?>
+<?php //get_template_part("main-pages-sections/front-page/mission-section"); ?>
+<?php //get_template_part("main-pages-sections/front-page/event-section"); ?>
+<?php //get_template_part("main-pages-sections/front-page/news-section"); ?>
 
 <?php get_footer(); ?>
