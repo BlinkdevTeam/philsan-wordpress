e3b4ecc249a2626bd02e38c6905e1d736c89d93d
KenMirwell <kenmirwell@gmail.com>
2026-05-08 09:22:09 +0800

    update

--- template/template-sample-payment-page.php
+++ template/template-sample-payment-page.php
@@ -19,10 +19,10 @@ if (isset($_POST['payment_type'])) {
     $type = $_POST['payment_type'];
 
     if ($type === 'member') {
-        $amount = 1000; // ₱10
+        $amount = 5000; // ₱50
         $description = "Member Registration";
     } else {
-        $amount = 2000; // ₱20
+        $amount = 10000; // ₱100
         $description = "Non-Member Registration";
     }
 
@@ -167,11 +167,11 @@ if (isset($_POST['payment_type'])) {
     >
 
     <button type="submit" name="payment_type" value="member">
-        Member – ₱10
+        Member – ₱50
     </button>
 
     <button type="submit" name="payment_type" value="non_member">
-        Non-Member – ₱20
+        Non-Member – ₱100
     </button>
 
 </form>
