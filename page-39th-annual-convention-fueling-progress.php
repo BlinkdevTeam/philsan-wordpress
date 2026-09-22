7d1deba0c3947129936910d5bcd3c707ee6092bf
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2026-09-22 10:05:55 +0800

    c

--- page-39th-annual-convention-fueling-progress.php
+++ page-39th-annual-convention-fueling-progress.php
@@ -11,7 +11,7 @@
         <?php get_template_part('39th-event-page/hero-section'); ?>
     </div>
     <?php get_template_part('39th-event-page/about-section'); ?>
-    <?php get_template_part('39th-event-page/speakers-section'); ?>
+    <?php //get_template_part('39th-event-page/speakers-section'); ?>
     <?php get_template_part('39th-event-page/sponsors-section'); ?>
     <?php get_template_part('39th-event-page/schedule-section'); ?>
     <?php wp_footer(); ?>
